58e1d72ef3ca83875fc53bf2d467183484dcd963
VarginDimitry <dimitry.vargin@yandex.ru>
2020-10-05 23:20:30 +0300

    V.9

--- src/main4.cpp
+++ src/main4.cpp
@@ -1,8 +1,5 @@
 #include "task4.h"
 
 int main(){
-    char *s1 = "0";
-    char *s2 = "0";
-    cout << sum(s1, s2);
     return 0;
 }
--- src/main5.cpp
+++ src/main5.cpp
@@ -1,14 +1,5 @@
 #include "task5.h"
 
 int main(){
-    /*
-    int word_number = 0;
-    char **words;
-    char *s = "qwe asd xzcs";
-    split(&words, &word_number, s, ' ');
-    for (int i = 0; i < word_number; ++i) {
-        cout << words[i] << endl;
-    }
-     */
     return 0;
 }
