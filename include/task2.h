f1986bbd95e24031f2acbcafba6d3dc23f45029e
Ramazan <ramazankka220@gmail.com>
2020-10-05 20:52:59 +0300

    made task 2

--- include/task2.h
+++ include/task2.h
@@ -1,1 +1,12 @@
 #pragma once
+#pragma once
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+bool checkPrime(unsigned int value);
+unsigned long long nPrime(unsigned n);
+unsigned long long nextPrime(unsigned long long value);
---
+++ src/task2.cpp
@@ -0,0 +1,54 @@
+#include "task2.h"
+
+bool checkPrime(unsigned int value)
+{
+    if (value > 1)
+    {
+        for (int i = 2; i < value; i++)
+            if (value % i == 0)
+                return false;
+        return true;
+    }
+    else
+        return false;
+};
+
+unsigned long long nPrime(unsigned n)
+{
+    int i, j;
+    int a = 0;
+    for (i = 2;; i++)
+    {
+
+        for (j = 2; j * j <= i; j++)
+            if ((i % j) == 0) break;
+
+        if (j * j > i) {
+            a++;
+            if (a == n) {
+                return i;
+                break;
+            }
+        }
+    }
+
+};
+
+unsigned long long nextPrime(unsigned long long value)
+{
+    int i, j;
+    int a = 0;
+    for (i = value;; i++) {
+
+        for (j = 2; j * j <= i; j++)
+            if ((i % j) == 0) break;
+
+        if (j * j > i) {
+            a++;
+            if (a == 1) {
+                return i - value;
+                break;
+            }
+        }
+    }
+};
