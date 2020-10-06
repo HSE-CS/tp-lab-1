9a6c49888b8ee266187676b3f13b5937df7377a2
Ksuvot <ksuvot36@yandex.ru>
2020-10-06 17:26:50 +0300

    update

--- src/task4.cpp
+++ src/task4.cpp
@@ -1,54 +1,56 @@
-#include <iostream>
-#include <cstring>
 #include "task4.h"
 
 using namespace std;
 
-char * sum(char *x, char *y)
+char *sum(char *x, char *y)
 {
-    int max_length = max(strlen(x), strlen(y));
     int count_x = strlen(x);
     int count_y = strlen(y);
-
-    char * str = new char[max_length];
-    if (count_x > count_y)
+	
+    int max_length;
+    if (count_x >= count_y)
+        max_length = count_x;
+    else
+        max_length = count_y;
+	
+    char *temp = new char[max_length];
+    if (count_x < count_y)
 	{
-        for (int i = 0; i < count_x - count_y; ++i)
-            str[i] = '0';
-        for (int j = 0; j < count_y; ++j)
-            str[count_x - count_y + j] = y[j];
-        y = str;
+        for (int i = 0; i < count_y - count_x; i++)
+            temp[i] = '0';
+        for (int i = 0; i < count_x; i++)
+            temp[i + count_y - count_x] = x[i];
+        x = temp;
     }
-    if (count_y > count_x)
+    if (count_y < count_x)
 	{
-        for (int i = 0; i < count_y - count_x; ++i)
-            str[i] = '0';
-        for (int j = 0; j < count_x; ++j)
-            str[count_y - count_x + j] = x[j];
-        x = str;
-    }
-    delete [] str;
-
-    int num = 0;
-    char * new_str = new char[max_length+1];
-    for (int i = max_length-1; i > -1; --i)
-	{
-        new_str[i] = x[i] + y[i] + num - '0';
-        num = 0;
-        if (new_str[i] > 57)
-		{
-            new_str[i] = new_str[i] - 10;
-            num++;
-            if (i == 0 && num > 0)
-			{
-                for (int i = max_length; i > -1; --i)
-                    new_str[i] = new_str[i-1];
-                new_str[0] = '1';
-            }
-        }
-        if (i == 0 && num == 0)
-            new_str[max_length] = '\0';
+        for (int i = 0; i < count_x - count_y; i++)
+            temp[i] = '0';
+        for (int i = 0; i < count_y; i++)
+            temp[i + count_x - count_y] = y[i];
+        y = temp;
     }
 	
-    return new_str;
+	int num = 0;
+	char *answer = new char[max_length + 2];
+    for (int i = max_length - 1; i >= 0; i--)
+	{
+        answer[i] = x[i] + y[i] + num - 48;
+        num = 0;
+        if (answer[i] >= 58)
+		{
+            num++;
+            answer[i] -= 10;
+            if (i == 0)
+			{
+                for (int j = max_length; j >= 0; j--)
+                    answer[j] = answer[j - 1];
+                answer[0] = '1';
+            }
+        }
+        if (num == 0 && i == 0)
+            answer[max_length] = '\0';
+    }
+    answer[max_length + 1] = '\0';
+    return answer;
 }
