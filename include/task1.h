ec85ea412319632f09f030f85b86edaf1fca96f1
Osmanov Islam <osmanv152@gmail.com>
2020-10-06 23:05:31 +0300

    Testing 1 and 2

---
+++ include/task1.h
@@ -0,0 +1,20 @@
+//
+// Created by islam on 15.09.2020.
+//
+
+#ifndef WORK_TASK1_H
+#define WORK_TASK1_H
+
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+unsigned long findValue(unsigned int min,unsigned max);
+
+
+
+#endif //WORK_TASK1_H
---
+++ src/main2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+#include "task2.h"
+using namespace std;
+
+int main()
+{
+    cout<<checkPrime(2)<<endl;
+    cout<<nPrime(500)<<endl;
+    cout<<nextPrime(1031)<<endl;
+    return 0;
+}
