bbe59d9e1f74d5784a4a3b1f45dadfe7acc7e826
Polina Ozhiganova <44429468+DoktaPola@users.noreply.github.com>
2020-10-06 13:52:53 +0300

    add headers

---
+++ include/task2.h
@@ -0,0 +1,8 @@
+#ifndef LAB_1_TASK2_H
+#define LAB_1_TASK2_H
+
+bool checkPrime(unsigned int value); //- проверка числа на простоту.
+unsigned long long nPrime(unsigned n); //- нахождение n-ого простого числа (в ряду).
+unsigned long long nextPrime(unsigned long long value); //- нахождение ближайшего следующего простого числа к value.
+
+#endif //LAB_1_TASK2_H
---
+++ include/task4.h
@@ -0,0 +1,9 @@
+#ifndef LAB_1_TASK4_H
+#define LAB_1_TASK4_H
+
+#include <cstring>
+#include <string>
+
+char * sum(char *x, char *y);
+
+#endif //LAB_1_TASK4_H
