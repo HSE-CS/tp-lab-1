50035dd3e18cdcbea764235aa2b4d8fee073b99a
wityat <ttrttr449@gmail.com>
2020-10-06 15:36:15 +0300

    Shatilov Victor

--- src/main4.cpp
+++ src/main4.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cstring>
 #include "task4.h"
 using namespace std;
 
 int main(){
-    cout << sum("123", "12") << endl;
+    char* res = sum("123456789", "000000001");
+    for (int i = 0; i < strlen(res); ++i) {
+        cout << res[i];
+    }
     return 0;
 }
 
--- src/task4.cpp
+++ src/task4.cpp
@@ -15,37 +15,39 @@ char * sum(char *x, char *y){
     else
         length = size_y + 1;
 
-    int a[size_x];
+    int a[size_x] = {0};
     int c = 0;
-    for(int i = size_x; i >= 0; i --){
+    for(int i = size_x-1; i >= 0; i --){
         a[i] = x[c] - '0';
         c++;
     }
 
-    int b[length];
+    int b[length] = {0};
     c = 0;
-    for(int i = size_y; i >= 0; i --){
-        b[i] = atoi(&y[c]);
+    for(int i = size_y-1; i >= 0; i --){
+        b[i] = y[c] - '0';
         c++;
     }
 
     for (int ix = 0; ix < length; ix++)
     {
-        b[ix] += a[ix]; // суммируем последние разряды чисел
-        b[ix + 1] += (b[ix] / 10); // если есть разряд для переноса, переносим его в следующий разряд
-        b[ix] %= 10; // если есть разряд для переноса он отсекается
+        if (ix < size_x) {
+            b[ix] += a[ix]; // суммируем последние разряды чисел
+            b[ix + 1] += (b[ix] / 10); // если есть разряд для переноса, переносим его в следующий разряд
+            b[ix] %= 10; // если есть разряд для переноса он отсекается
+        }else break;
     }
 
     if (b[length - 1] == 0)
         length--;
 
-    char res[length];
+    char *res = new char[length];
     c = 0;
 
 
 
-    for(int i = length; i >= 0; --i) {
-        std::cout << b[i] << std::endl;
+    for(int i = length-1; i >= 0; --i) {
+//        std::cout << b[i] << std::endl;
         res[c] = (char) (b[i] + '0');
         c++;
     }
