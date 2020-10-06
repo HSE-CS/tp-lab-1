5dc00bab6490799434301883aaf6983ae98b290c
Ramazan <ramazankka220@gmail.com>
2020-10-06 16:04:32 +0300

    fixes of 1 task

--- include/task1.h
+++ include/task1.h
@@ -5,5 +5,5 @@
 #include <ctime>
 #include <string>
 
-int mas[20] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12 ,13, 14, 15, 16, 17, 18, 19, 20 };
+//int mas[20] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12 ,13, 14, 15, 16, 17, 18, 19, 20 };
 unsigned long findValue(unsigned int min, unsigned max);
