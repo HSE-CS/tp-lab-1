297a102f6d59cfce0b0cea1f21227a10de2ff95c
wityat <ttrttr449@gmail.com>
2020-10-06 21:03:22 +0300

    Shatilov Victor add task5

--- src/main5.cpp
+++ src/main5.cpp
@@ -1,4 +1,18 @@
-//
-// Created by victor on 06.10.2020.
-//
+#include <iostream>
+#include <cstring>
+#include "task5.h"
+using namespace std;
+
+int main(){
+    char **result;
+    int N = 0;
+    split(&result, &N, "123,456,789,", ',');
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < strlen(result[i]); ++j) {
+            cout << result[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
 
--- src/task5.cpp
+++ src/task5.cpp
@@ -1,4 +1,26 @@
-//
-// Created by victor on 06.10.2020.
-//
+#include <cstring>
 
+void split(char ***result, int *N, char *buf, char ch){
+    int last_i = -1;
+    for(int i = 0; i < strlen(buf); i++){
+        if (buf[i] == ch && i != 0 && i != strlen(buf)-1) {
+            ++*N;
+        }
+    }
+    ++*N;
+    *result = new char *[*N];
+    int n = 0, c = 0;
+    for(int i = 0; i <= strlen(buf); i++){
+        if (buf[i] == ch || (i == strlen(buf) && *N!=0)){
+            (*result)[n] = new char [i-last_i-1];
+            for (int j = last_i+1; j < i; ++j) {
+                (*result)[n][c] = buf[j];
+                c++;
+//                std::cout << (*result)[n][j];
+            }
+            last_i=i;
+            n++;
+            c=0;
+        }
+    }
+}
