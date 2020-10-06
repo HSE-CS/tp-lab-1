e4b972c308d16fe106a0b50e75db9328c003c741
Ksuvot <ksuvot36@yandex.ru>
2020-10-06 17:02:45 +0300

    update

--- src/main1.cpp
+++ src/main1.cpp
@@ -2,9 +2,5 @@
 
 int main()
 {
-    unsigned int min;
-    unsigned int max;
-    cin >> min >> max;
-    cout << findValue(min, max);
     return 0;
 }
--- src/task1.cpp
+++ src/task1.cpp
@@ -6,19 +6,24 @@ using namespace std;
 bool check(unsigned int min, unsigned max, unsigned int num)
 {
     for (unsigned int i = min; i <= max; ++i)
-    {
+	{
         if (num % i != 0)
+		{
             return false;
+        }
         if (i == max)
+		{
             return true;
+        }
     }
 }
+
 
 unsigned long findValue(unsigned int min, unsigned max)
 {
     unsigned int num = max;
     while(!check(min, max, num))
-    {
+	{
         ++num;
     };
     return num;
