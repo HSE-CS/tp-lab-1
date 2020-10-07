7c4595ee49b77c0df3514b6111254972d15b1e1b
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-10-07 09:54:06 +0300

    Update task5.cpp

--- src/task5.cpp
+++ src/task5.cpp
@@ -1,23 +1,23 @@
+
 #include "task5.h"
 #include <cstring>
 
 
-
-void split(char*** result, int* N, char* buf, char ch){
-    const int len = 1000 ;
-
-    (*result) = new char* [len]();
-    for (int i = 0; i < len; i++) {
-        (*result)[i] = new char [len]();
-    }
-    char* tmp_char;
-    string s;
-    s += ch;
-    s += '\0';
-    const char* sep = s.c_str();
-    tmp_char = strtok(buf, sep);    // âûçîâåì ôóíêöèþ strtok äëÿ ðàçäåëåíèÿ ñòðîêè â buff ïî ïðîáåëàì
-    while (tmp_char != NULL) {  ///âûâîäèì ÷àñòè ïîêà îíè ñóùåñòâóþò
+ void split(char*** result, int* N, char* buf, char ch) {
+        const int len = 1000;
+        (*result) = new char* [len]();
+        for (int i = 0; i < len; i++) {
+            (*result)[i] = new char[len]();
+        }
+        char* tmp_char;
+        string s;
+        s += ch;
+        s += '\0';
+        const char* sep = s.c_str();
+        char* next_token1 = NULL;
+        tmp_char = strtok_s(buf, sep,&next_token1);    // âûçîâåì ôóíêöèþ strtok äëÿ ðàçäåëåíèÿ ñòðîêè â buff ïî ïðîáåëàì
+        while (tmp_char != NULL) {  ///âûâîäèì ÷àñòè ïîêà îíè ñóùåñòâóþò
             (*result)[(*N)++]=tmp_char;
-            tmp_char = strtok(NULL, sep);    ///получим следующую часть
+            tmp_char = strtok_s(NULL, sep, &next_token1);    ///получим следующую часть
         }
 }
