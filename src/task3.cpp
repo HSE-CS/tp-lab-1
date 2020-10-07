2bf0df8a4d3359781fc71ba9acf68dca9a69bffa
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2020-10-07 16:24:59 +0300

    Task 3 done

---
+++ src/task3.cpp
@@ -0,0 +1,26 @@
+#include "task3.h"
+#include "task2.h"
+
+unsigned long long sumPrime(unsigned int hbound)
+{
+	bool aPrime;
+	unsigned int index = 1, sent = 3, sum = 0;
+	unsigned int *primeNumbers;
+
+	primeNumbers = new unsigned int[hbound];
+	primeNumbers[0] = 2;
+	sum += 2;
+
+	while (sent < hbound) {
+		aPrime = realPrimeStorage(primeNumbers, sent, index);
+		if (aPrime) {
+			sum += primeNumbers[index];
+			index++;
+		}
+		sent += 2;
+	}
+
+	delete[] primeNumbers;
+
+	return sum;
+}
