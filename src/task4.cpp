feef85438012a5f830049e8ea865e44fc701942e
Ramazan <ramazankka220@gmail.com>
2020-10-07 11:04:43 +0300

    fixes of 4 task

--- src/task4.cpp
+++ src/task4.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "task4.h"
 #include <stdlib.h>
 #include <cstring>
@@ -6,26 +7,71 @@ using namespace std;
 
 char* sum(char* x, char* y)
 {
-	long long int number;
-	int amount = 0;
-	long long int first = atoll(x);
-	long long int second = atoll(y);
-	long long int answer = first + second;
+	unsigned long long int k = 1;
+	unsigned long long int first = 0;
 
-	number = answer;
+	int size_a = strlen(y);
+	int size_b = strlen(x);
 
-	for (int i = 0;; i++)
+	int i = 0;
+	int ix;
+
+	int length;
+	if (size_a > size_b)
+		length = size_a + 1;
+	else
+		length = size_b + 1;
+
+	int* a = new int[length];
+	int* b = new int[length];
+
+	for (i = 0; i < size_b; i++)
 	{
-		amount = amount + 1;
-		number = number / 10;
-		if (number == 0)
-			break;
+		b[i] = x[i] - 48;
 	}
-	char* mass_answer = new char[amount];
 
-	std::string str = std::to_string(answer);
+	while (i < length)
+	{
+		b[i] = 0;
+		i++;
+	}
 
-	std::strcpy(mass_answer, str.c_str());
+	for (i = 0; i < size_a; i++)
+	{
+		a[i] = y[i] - 48;
+	}
+
+	while (i < length)
+	{
+		a[i] = 0;
+		i++;
+	}
+
+	for (ix = 0; ix < length; ix++)
+	{
+		b[ix] += a[ix];
+		b[ix + 1] += (b[ix] / 10);
+		b[ix] %= 10;
+	}
+
+	if (b[length + 1] == 0)
+		length--;
+
+	int z;
+
+	for (ix = 0; ix < (length) / 2; ix++)
+	{
+		z = b[ix];
+		b[ix] = b[length - ix - 1];
+		b[length - ix - 1] = z;
+	}
+
+	char* mass_answer = new char[length + 1];
+	mass_answer[length] = '\0';
+	for (ix = 0; ix < length; ix++)
+	{
+		mass_answer[ix] = b[ix] + '0';
+	};
 
 	return mass_answer;
 };
