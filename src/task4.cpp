05e02674a32270b328ac11251eadda0ae96e127b
alexgiving <alexgiving@mail.ru>
2020-10-09 10:25:44 +0300

    task 4 done???

--- src/task4.cpp
+++ src/task4.cpp
@@ -4,7 +4,6 @@
 
 #include "task4.h"
 #include <cstring>
-#include <string.h>
 #include <algorithm>
 using namespace std;
 
@@ -13,34 +12,35 @@ char *sum(char *x, char *y){
     unsigned int lenX = strlen(x);
     unsigned int lenY = strlen(y);
     unsigned int maxLength = max(lenX, lenY);
-    char *buf = new char[maxLength + 1];
-    char *t = new char[maxLength];
     bool overflow = false;
     if (lenX > lenY){
-        for (unsigned int i = 0; i < lenX - lenY; i++) t[i] = '0';
-        for (unsigned int j = 0; j < lenY; j++) t[j + lenX - lenY] = y[j];
-        y = t;
+        char *buf = new char[lenX - lenY];
+        for (unsigned int i = {0}; i < lenX - lenY; i++) buf[i] = '0';
+        buf[lenX - lenY] = {0};
+        y = strcat(buf, y);
+        delete [] buf;
+    } else if (lenY > lenX){
+        char *buf = new char[maxLength + 1];
+        for (unsigned int i = {0}; i < lenY - lenX; i++) buf[i] = '0';
+        buf[lenY - lenX] = {0};
+        x = strcat(buf, x);
+        delete [] buf;
     }
-    if (lenY > lenX){
-        for (unsigned int i = 0; i < lenY - lenX; i++) t[i] = '0';
-        for (unsigned int j = 0; j < lenX; j++) t[j + lenY - lenX] = x[j];
-        x = t;
-    }
-    delete [] t;
-    for (int i = (int)(maxLength - 1); i >= 0; i--) {
-        buf[i] = (char)(x[i] + y[i] + overflow - '0');
-        overflow = false;
-        if (buf[i] > '9'){
-            buf[i] = (char)(buf[i] - 10);
-            overflow = true;
-            if (0 == i){
-                char* buf1 = new char[maxLength/2];
-                buf1[0]='1';
-                buf = strcat(buf1,buf);
-            }
+    char *temp = new char[maxLength + 2];
+    temp[maxLength + 1] = {0};
+    for (unsigned int i = {0}; i <= maxLength; i++) {
+        if (i == maxLength) {
+            temp[0] = '0' + overflow;
+            break;
         }
-        if (0 == i && 0 == overflow) buf[maxLength] = '\0';
+        temp[maxLength - i] = x[maxLength - i - 1] + y[maxLength - i - 1] + overflow - '0';
+        overflow = false;
+        if (temp[maxLength - i] > '9') {
+            overflow = true;
+            temp[maxLength - i] = temp[maxLength - i] - 10;
+        }
     }
-    return buf;
+    if (temp[0] == '0') return temp + 1;
+    return temp;
 }
 
