0d7f6a6ca579db6896b1a0b89bd82625ab30655d
GlebNeshchetkin <glebneshchetkin@gmail.com>
2020-10-10 18:11:09 +0300

    4Test

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,14 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include "task1.h"
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <malloc.h>
-#include <string.h>
-#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
 
 int main()
 {
-	unsigned int result = 0;
-	result = findValue(1, 10);
-	printf("%d", result);
 	return 0;
 }
