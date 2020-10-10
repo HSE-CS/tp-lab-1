f840eed45368158b3f1cc16e6ea5214d70032a53
LukichevaPolina <lukichevapolina42@gmail.com>
2020-10-10 22:53:13 +0300

    Lukicheva Polina Lab1

--- src/task5.cpp
+++ src/task5.cpp
@@ -6,7 +6,7 @@ void split(char*** result, int* N, char* buf, char ch)
 	for (int i = 0; i < strlen(buf); i++)
 		if (buf[i] == ch)
 			++*N;
-
+	*N++;
 	(*result) = new char* [*N + 1];
 	for (int i = 0; i <= *N; i++) {
 		(*result)[i] = new char[strlen(buf)];
