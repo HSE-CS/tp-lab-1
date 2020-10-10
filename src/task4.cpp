509fb5a3a99fc95bc167692a5f561962a52812cd
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2020-10-10 22:48:31 +0300

    fixed max_len + 1 in task4

--- src/task4.cpp
+++ src/task4.cpp
@@ -3,6 +3,12 @@ char * sum(char *x, char *y)
 {
   int len_x = strlen(x);
   int len_y = strlen(y);
+  // cout << len_x << " " << len_y << endl;
+
+  // for (int i = 0; y[i] != '\0'; i++)
+  //   cout <<y[i];
+  // cout << endl;
+
   int boofer = 0;
   int max_len = len_x > len_y ? len_x: len_y;
   char *new_str = (char *)malloc(max_len+1);
@@ -10,38 +16,34 @@ char * sum(char *x, char *y)
   int first_num = 0;
   int second_num = 0;
   int next_num = 0;
-  for (int i = 0; i < max_len; i++)
+  for (int i = 0; i < max_len + 1; i++)
   {
-    if (len_x < i)
-
+    if (len_x <= i)
       first_num = 0;
 
     else
-      first_num = x[max_len - i - 1] - '0';
+      first_num = x[len_x - i - 1] - '0';
 
-
-      if (len_y < i)
-
-        second_num = 0;
-
-      else
-        second_num = y[max_len - i - 1] - '0';
+    if (len_y <= i)
+      second_num = 0;
+    else
+      second_num = y[len_y - i - 1] - '0';
 
     next_num = first_num + second_num + boofer;
 
     boofer = next_num / 10;
     next_num = next_num % 10;
-
     new_str[i] = next_num + '0';
+    // cout << first_num << " " << second_num << " " << boofer << " " << new_str[i] << endl;
 
   }
 
-  if (new_str[max_len-1] == '0')
+  if (new_str[max_len] == '0')
        --max_len;
 
-   reverse(new_str, new_str + max_len);
+   reverse(new_str, new_str + max_len + 1);
 
-   new_str[max_len] = '\0';
+   new_str[max_len + 1] = '\0';
    if (max_len == 0)
    {
        new_str[0] = '0';
