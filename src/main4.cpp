49a789bb586bdc5d7b93186e23175afe5bd34ff7
LukichevaPolina <lukichevapolina42@gmail.com>
2020-10-10 21:44:28 +0300

    Lukicheva Polina Lab1

--- src/main4.cpp
+++ src/main4.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "task4.h"
-#include <cstring>
 
 int main() {
 	char a[] = "123456789";
 	char b[] = "000000001";
-	char* c = sum(a, b);
+	char* c = new char;
+	c = sum(a, b);
 	for (int i = 0; i < strlen(c); i++)
 		std::cout << c[i];
 
