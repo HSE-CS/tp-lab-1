770061b5573e0f3bb845c4eb9e54e80f5b4f8a21
LukichevaPolina <lukichevapolina42@gmail.com>
2020-10-10 22:25:36 +0300

    Lukicheva Polina Lab1

--- src/main4.cpp
+++ src/main4.cpp
@@ -3,8 +3,8 @@
 #include <cstring>
 
 int main() {
-	char a[] = "99999999999999999999";
-	char b[] = "1";
+	char* a = "99999999999999999999";
+	char* b = "1";
 	char* c = new char[strlen(a)+strlen(b)];
 	c = sum(a, b);
 	for (int i = 0; i < strlen(c); i++)
