9c01dcc2e02705c82beed21107f224cd57e0011a
danek0100 <71225718+danek0100@users.noreply.github.com>
2020-10-10 12:41:16 +0300

    debug2

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,4 +1,5 @@
 #include "task3.h"
+#include "task2.h"
 #include <cmath>
 #include <stdio.h>
 #include <algorithm>
@@ -17,23 +18,4 @@ unsigned long long sumPrime(unsigned int hbound)
         sum += now;
     }
     return sum;
-}
-
-unsigned long long nextPrime(unsigned long long value)
-{
-    unsigned long long k = value + 1;
-    while (!checkPrime(k))
-        k++;
-    return k;
-}
-
-bool checkPrime(unsigned int value)
-{
-    if (value <= 1)
-        return 0;
-
-    for (int i = 2; i <= sqrt(value); i++)
-        if (value % i == 0)
-            return false;
-    return true;
 }
