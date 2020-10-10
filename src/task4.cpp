2088ec014eabbe7ebb4d60d30855901ab40d337d
natasha <igum.natasha@gmail.com>
2020-10-10 21:53:11 +0300

    Fix bug in task4.cpp

--- src/task4.cpp
+++ src/task4.cpp
@@ -14,7 +14,7 @@ char* sum(char* x, char* y)
 	else
 		size = strlen(y) + 1;
 	char* result = new char[size];
-	for (int i = 0;i < size;i++)
+	for (int i = 0;i <= size;i++)
 	{
 		result[i] = '\0';
 	}
@@ -43,7 +43,7 @@ char* sum(char* x, char* y)
 	int i;
 	if (result[size - 1] == '0')
 		size -= 1;
-	for (int i = 0;i < size;i++)
+	for (int i = 0;i <= size;i++)
 	{
 		result2_0[i] = '\0';
 	}
