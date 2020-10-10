1c6f6e23ee9b1180b7923988200e94fcd210eed4
valvarl <valvarl@ya.ru>
2020-10-11 00:43:30 +0300

    task1

--- src/main1.cpp
+++ src/main1.cpp
@@ -2,3 +2,11 @@
 // Created by freeb on 11.10.2020.
 //
 
+#include <iostream>
+#include "task1.h"
+
+int main() {
+//    unsigned long res=findValue(1,20);
+//    std::cout << res;
+    return 0;
+}
--- src/task1.cpp
+++ src/task1.cpp
@@ -2,3 +2,12 @@
 // Created by freeb on 11.10.2020.
 //
 
+#include "task1.h"
+
+unsigned long findValue(unsigned int min, unsigned int max) {
+    unsigned long result = 1;
+    for (unsigned int w = min; w <= max; w++) {
+        result *= w;
+    }
+    return result;
+}
