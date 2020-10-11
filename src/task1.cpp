c1d5a60c0a9c0a7ddc2b1f5db3521a34ff39664a
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-10-11 21:24:55 +0300

    b75

--- src/task1.cpp
+++ src/task1.cpp
@@ -8,7 +8,7 @@ unsigned int min = 1;
 unsigned max = 1;
 
 unsigned long findValue(unsigned int min, unsigned max) {
-    while (max <= 20)
+    while (max <=20)
     {
         if (min % max == 0) y++;
         else { min++; max = 1; }
