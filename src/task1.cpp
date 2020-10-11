18366d80fe1b1f30400c3c1f9bc639ef218e552b
Leonidek23 <rad.shabarshin@mail.ru>
2020-10-11 23:39:52 +0300

    OWO

--- src/task1.cpp
+++ src/task1.cpp
@@ -6,7 +6,7 @@
 unsigned long findValue(unsigned int min, unsigned int max) {
 	unsigned int i = max+1;
 	int ostatok = 0;
-	while ostatok != 0{
+	while (ostatok != 0){
 		ostatok = 0;
 		for (unsigned int j = min; j < max + 1; j++) {
 			ostatok = ostatok + i % j;
