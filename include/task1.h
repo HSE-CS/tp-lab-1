ea5d1dbe413071ca5876a0acf6110cfa20a99f52
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-10-11 21:34:05 +0300

    b75

--- include/task1.h
+++
@@ -1,6 +0,0 @@
-#define TASK1_H
-#ifdef TASK1_H
-
-unsigned long findValue(unsigned int min, unsigned max);
-
-#endif // TASK1_H
--- src/main1.cpp
+++
@@ -1,8 +0,0 @@
-#include "task1.h"
-#include <stdio.h>
-#include <iostream>
-
-int main() {
-
-    return 0;
-}
