d61e88d7be420bc7a5a748fdc5175f7d64ec6608
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-10-11 23:11:30 +0300

    b75-2

---
+++ include/task2.h
@@ -0,0 +1,8 @@
+#define TASK2_H
+#ifdef TASK2_H
+
+bool checkPrime(unsigned int value);
+unsigned long long nPrime(unsigned int n);
+unsigned long long nextPrime(unsigned long long value);
+
+#endif // TASK2_H
---
+++ src/main2.cpp
@@ -0,0 +1,10 @@
+#include "task2.h"
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+
+int main() {
+
+
+    return 0;
+}
