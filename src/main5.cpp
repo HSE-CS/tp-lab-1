43938ddc64402cc7d68660a2c19c24dbbfa474b7
valvarl <valvarl@ya.ru>
2020-10-11 05:23:13 +0300

    empty main task5

--- src/main5.cpp
+++ src/main5.cpp
@@ -5,10 +5,10 @@
 #include "task5.h"
 
 int main() {
-    char *buf="123,456,789";
-    int N=0;
-    char **result=nullptr;
-    split(&result, &N, buf, ',');
+//    char *buf="123,456,789";
+//    int N=0;
+//    char **result=nullptr;
+//    split(&result, &N, buf, ',');
     return 0;
 }
 
