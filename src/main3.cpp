152df43d07a7152a5982e7b11120f1ef4cc6d85e
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-10-11 23:54:09 +0300

    b75-3

---
+++ src/main3.cpp
@@ -0,0 +1,10 @@
+#include "task3.h"
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+
+int main() {
+
+
+    return 0;
+}
---
+++ src/task3.cpp
@@ -0,0 +1,19 @@
+#include "task3.h"
+
+using namespace std;
+unsigned int hbound = 1999999;
+
+
+unsigned long long sumPrime(unsigned int hbound) {
+	sum = 0;
+	for (i = 2; i <= hbound; i++) {
+		if (checkPrime(i) == true) {
+			sum += i;
+		}
+	}
+
+	return sum;
+
+
+
+}
