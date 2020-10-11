706ac9b76fa6670ed6567afde348b5603e6efc48
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-10-11 22:14:43 +0300

    b75

---
+++ src/main1.cpp
@@ -0,0 +1,8 @@
+#include "task1.h"
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
---
+++ src/task1.cpp
@@ -0,0 +1,18 @@
+#include "task1.h"
+#include <iostream>
+
+
+using namespace std;
+
+unsigned int min = 1;
+unsigned max = 1;
+
+unsigned long findValue(unsigned int min, unsigned max) {
+    while (max <=20)
+    {
+        if (min % max == 0) y++;
+        else { min++; max = 1; }
+    }
+    cout << min << endl;
+
+}
