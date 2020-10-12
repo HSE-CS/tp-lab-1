9aae994066b93636da745bb692a1a24f5db0c4c3
Lavrov-Artyom <artem3sandy@gmail.com>
2020-10-12 15:55:51 +0300

    Task2

---
+++ src/main2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "task2.h"
+
+using namespace std;
+
+int main() {
+    cout << checkPrime(2) << endl;
+    cout << checkPrime(3) << endl;
+    cout << checkPrime(12) << endl << endl;
+
+    cout << nPrime(6) << endl;
+    cout << nPrime(500) << endl << endl;
+
+    cout << nextPrime(1031) << endl;
+    cout << nextPrime(3559) << endl;
+    cout << nextPrime(2) << endl << endl;
+
+}
---
+++ src/task2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "task2.h"
+#include <cmath>
+
+using namespace std;
+
+bool checkPrime(unsigned int value) {
+    for (unsigned int i = 2; i <= sqrt(value); i++)
+        if (value % i == 0)
+            return false;
+    return true;
+}
+
+unsigned long long nPrime(unsigned n) {
+    unsigned a = 0;
+    for (unsigned int i = 2; a != n; ++i) {
+        if (checkPrime(i)) {
+            a++;
+        }
+        if (a == n) {
+            return i;
+        }
+    }
+}
+
+unsigned long long nextPrime(unsigned long long value) {
+    for (++value; true; ++value) {
+        if (checkPrime(value)) {
+            return value;
+        }
+    }
+
+}
