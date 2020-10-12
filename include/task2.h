b60557b26886f2d6261a4fefca0e6d900624b430
Vladimir <vl.shev@list.ru>
2020-10-12 17:46:20 +0300

    One more time

--- include/task2.h
+++ include/task2.h
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string.h>
 
-bool checkPrime(unsigned int value)
+bool checkPrime(unsigned int value);
 unsigned long long nPrime(unsigned n);
 unsigned long long nextPrime(unsigned long long value);
