9ace9a564cd2b83b0d53b5c0489606569efd9999
jswark <swark.a.j@gmail.com>
2020-10-12 19:18:04 +0300

    fix main

--- src/main4.cpp
+++ src/main4.cpp
@@ -6,11 +6,7 @@ using namespace std;
 int main() {
     char *x1="123456789";
     char *y1="000000001";
-    char *y = new char [strlen(y1) + 1];
-    char *x = new char [strlen(x1) + 1];
-    strcpy(y,y1);
-    strcpy(x, x1);
     cout << sum(x, y) << endl;
-
+    
     return 0;
 }
