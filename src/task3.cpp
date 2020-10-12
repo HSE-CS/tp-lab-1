0006c3afb290ac6620639314f27eada7c6fa2901
Alexander Grachev <s89519011368@yandex.ru>
2020-10-12 23:15:46 +0300

    task3.1

---
+++ src/task3.cpp
@@ -0,0 +1,11 @@
+#include "../include/task2.h"
+
+unsigned long long sumPrime(unsigned int hbound){
+    long long sum = 0;
+    int i=1;
+    while(nPrime(i) < hbound){
+        sum += getPrime(i);
+        i++;
+    }
+    return sum;
+}
