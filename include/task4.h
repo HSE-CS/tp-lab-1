29ea9299c8b841b3bee63d216683b4ce18cf7985
Nikita Kasyanov <kasyanikita@gmail.com>
2020-10-12 08:42:01 +0300

    300 bucks

--- include/task4.h
+++ include/task4.h
@@ -6,5 +6,6 @@
 #define TASK1_TASK4_H
 
 char *sum(char *x, char *y);
+std::string Reverse(std::string a);
 
 #endif //TASK1_TASK4_H
--- src/task4.cpp
+++ src/task4.cpp
@@ -3,61 +3,49 @@
 //
 #include <cstring>
 #include <algorithm>
+#include <string>
 
+
+std::string Reverse(std::string a) {
+    std::string rev_str;
+    for (int i = a.size() - 1; i > -1; --i) {
+        rev_str += a[i];
+    }
+    return rev_str;
+}
 
 char *sum(char *x, char *y) {
-    int len_x = strlen(x);
-    int len_y = strlen(y);
-    char *result = new char[std::max(len_x, len_y)];
-    char *x_rev = new char[len_x];
-    char *y_rev = new char[len_y];
-    int i_x = len_x - 1;
-    int i_y = len_y - 1;
-    for (int i = 0; i < len_x; ++i) {
-        x_rev[i_x] = x[i];
-        --i_x;
-    }
-    for (int i = 0; i < len_y; ++i) {
-        y_rev[i_y] = y[i];
-        --i_y;
-    }
-    int i = 0;
-    int memory = 0;
-    while (i < len_x && i < len_y){
-        int a = x_rev[i] - '0';
-        int b = y_rev[i] - '0';
-        result[i] = ((a + b + memory) % 10) + '0';
-        memory = (a + b + memory) / 10;
+    std::string ans;
+    std::string a = x;
+    std::string b = y;
+    std::string rev_a = Reverse(a);
+    std::string rev_b = Reverse(b);
+    unsigned i = 0;
+    int tmp = 0;
+    while (i < rev_a.size() && i < rev_b.size()) {
+        int a = rev_a[i] - '0';
+        int b = rev_b[i] - '0';
+        ans += ((a + b + tmp) % 10) + '0';
+        tmp = (a + b + tmp) / 10;
         ++i;
     }
-    while (i < len_x) {
-        int a = x_rev[i] - '0';
-        result[i] = ((a + memory) % 10) + '0';
-        memory = (a + memory) / 10;
+    while (i < rev_a.size()) {
+        int a = rev_a[i] - '0';
+        ans += ((a + tmp) % 10) + '0';
+        tmp = (a + tmp) / 10;
         ++i;
     }
-    while (i < len_y) {
-        int a = y_rev[i] - '0';
-        result[i] = ((a + memory) % 10) + '0';
-        memory = (a + memory) / 10;
+    while (i < rev_b.size()) {
+        int b = rev_b[i] - '0';
+        ans += ((b + tmp) % 10) + '0';
+        tmp = (b + tmp) / 10;
         ++i;
     }
-    if (memory){
-        char* ans = new char[std::max(len_x, len_y) + 1];
-        ans[0] = memory + '0';
-        int j = std::max(len_x, len_y) - 1;
-        for (int i = 1; i < strlen(ans); ++i){
-            ans[i] = result[j];
-            --j;
-        }
-        return ans;
-    }else{
-        char* ans = new char[std::max(len_x, len_y)];
-        int j = std::max(len_x, len_y) - 1;
-        for (int i = 0; i < std::max(len_x, len_y); ++i){
-            ans[i] = result[j];
-            --j;
-        }
-        return ans;
+    if (tmp) {
+        ans += tmp + '0';
     }
+    ans = Reverse(ans);
+    char* ans_char = new char[ans.size()];
+    strcpy(ans_char, ans.c_str());
+    return ans_char;
 }
