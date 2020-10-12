8ffafa6da243abcddc3d290ae1e24efcefc93abd
Olesya Nikolaeva <63335416+olesyani@users.noreply.github.com>
2020-10-12 16:37:07 +0300

    updated

--- src/task4.cpp
+++ src/task4.cpp
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 
 char * sum(char *x, char *y) {
-    char * str;
+    char * str = new char[std::max(strlen(x), strlen(y)) + 1] {0};
     unsigned int l1 = strlen(x);
     unsigned int l2 = strlen(y);
     if (l1 > l2) {
         char tmp = 0;
-        str = (char*)malloc(l1 * (sizeof(char)));
         for (int i = 0; i < l2; i++) {
             char k = (x[l1 - i - 1] + y[l2 - i - 1]) - '0' - '0' + tmp;
             if (k < 10) {
@@ -30,14 +29,11 @@ char * sum(char *x, char *y) {
                 tmp = 1;
             }
         }
-        if (tmp == 1) {
-            str = (char *)realloc(str, l1 + 1);
+        if (tmp == 1)
             str[l1] = '1';
-        }
     }
     else {
         char tmp = 0;
-        str = (char*)malloc(l2 * (sizeof(char)));
         for (int i = 0; i < l1; i++) {
             char k = (x[l1 - i - 1] + y[l2 - i - 1]) - '0' - '0' + tmp;
             if (k < 10) {
@@ -60,10 +56,8 @@ char * sum(char *x, char *y) {
                 tmp = 1;
             }
         }
-        if (tmp == 1) {
-            str = (char *)realloc(str, l2 + 1);
+        if (tmp == 1)
             str[l2] = '1';
-        }
     }
     std::reverse(str, str + strlen(str));
     return str;
