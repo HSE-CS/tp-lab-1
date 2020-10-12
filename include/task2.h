9f88695e2e1f53c90c103d01523afd5eddc9f65e
Roma Balayan <roma1balayan@gmail.com>
2020-10-12 21:45:26 +0300

    Second task

--- include/task2.h
+++ include/task2.h
@@ -1,1 +1,5 @@
-NULL 
+#pragma once
+
+bool checkPrime(unsigned long long value);
+unsigned long long nPrime(unsigned int n);
+unsigned long long nextPrime(unsigned long long value);
--- src/task2.cpp
+++ src/task2.cpp
@@ -1,1 +1,30 @@
-Режим вывода команд на экран (ECHO) включен.
+#include "../include/task2.h"
+
+bool checkPrime(unsigned long long value)
+{
+	if (value <= 1) return false;
+	for (unsigned int i = 2; i < value; ++i)
+		if (value % i == 0) return false;
+	return true;
+}
+
+unsigned long long nPrime(unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	while (j != n)
+	{
+		++i;
+		if (checkPrime(i))
+			++j;
+	}
+	return i;
+}
+
+unsigned long long nextPrime(unsigned long long value)
+{
+	++value;
+	while (!checkPrime(value))
+		++value;
+	return value;
+}
