39ffb022fa59cce4b50d94e6e48763a09243af06
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2020-10-12 19:31:52 +0300

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 using namespace std;
 
-//int main()
-//{
-//	cout << checkPrime(78) << "\n";
-//	cout << checkPrime(5) << "\n";
-//	cout << nPrime(1) << "\n";
-//	cout << nPrime(7) << "\n";
-//	cout << nextPrime(7) << "\n";
-//	cout << nextPrime(20) << "\n";
-//	cout << nextPrime(100) << endl;
-//	return 0;
-//}
+int main()
+{
+	cout << checkPrime(78) << "\n";
+	cout << checkPrime(5) << "\n";
+	cout << nPrime(1) << "\n";
+	cout << nPrime(7) << "\n";
+	cout << nextPrime(7) << "\n";
+	cout << nextPrime(20) << "\n";
+	cout << nextPrime(100) << endl;
+	return 0;
+}
