b60d0a374c4ae90696176905f039d4f8b5b5ac6e
Alexander Grachev <s89519011368@yandex.ru>
2020-10-13 00:45:30 +0300

    task4.1

---
+++ src/main4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../include/task4.h"
+
+using namespace std;
+
+
+int main(){
+
+    cout << sum((char*)"33933", (char*)"33");
+
+
+    return 0;
+}
--- src/task4.cpp
+++ src/task4.cpp
@@ -1,5 +1,4 @@
 #include <string>
-#include <cstring>
 
 
 char* sum(char* x, char* y) {
@@ -10,9 +9,9 @@ char* sum(char* x, char* y) {
     unsigned int size;
 
     if (lenX > lenY)
-        unsigned int size = lenX + 2;
+        unsigned int size = lenX + 1;
     else
-        unsigned int size = lenY + 2;
+        unsigned int size = lenY + 1;
 
     char *sum = new char[size];
     sum[size] = '\0';
@@ -34,4 +33,27 @@ char* sum(char* x, char* y) {
         locY--;
         locSum--;
     }
+
+    while (locX >= 0){
+        int IntX = x[locX] - '0';
+
+        if (remainder + IntX >= 10)
+            remainder = IntX + remainder - 10 + '0';
+        sum[locSum] = (IntX + remainder) % 10 + '0';
+
+        locX--;
+        locSum--;
+    }
+
+    while (locY >= 0){
+        int IntY = x[locY] - '0';
+
+        if (remainder + IntY >= 10)
+            remainder = IntY + remainder - 10 + '0';
+        sum[locSum] = (IntY + remainder) % 10 + '0';
+
+        locY--;
+        locSum--;
+    }
+    return sum;
 }
