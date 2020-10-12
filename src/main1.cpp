06e757cb2541f1906babae43283772acac0317f7
Lavrov-Artyom <artem3sandy@gmail.com>
2020-10-12 15:21:38 +0300

    Task1

---
+++ src/main1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "task1.h"
+
+using namespace std;
+
+int main() 
+{
+    cout << search(1, 10) << endl;
+    cout << search(1, 20) << endl;
+}
---
+++ src/task1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "task1.h"
+
+using namespace std;
+
+bool cycle(unsigned int min, unsigned max, unsigned int number) 
+{
+    for (unsigned int i = min; i <= max; ++i) 
+    {
+        if (number % i != 0) 
+        {
+            return false;
+        }
+        if (i == max) 
+        {
+            return true;
+        }
+    }
+}
+
+
+unsigned long search(unsigned int min, unsigned max) 
+{
+    unsigned int number = max;
+    while (!cycle(min, max, number)) 
+    {
+        ++number;
+    };
+    return number;
+
+
+}
