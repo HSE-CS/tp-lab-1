df2449d52d182f6d7d8897e3feb34cc6f9a74975
Mushka Nikita <namushka@edu.hse.ru>
2020-10-13 02:33:20 +0300

    7

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,30 +1,12 @@
-#include <math.h>
-#include <iostream>
-
-bool checkPrime(unsigned int value){
-    for(long i=2;i<=sqrt(value);i++)
-        if(value%i==0)
-            return false;
-    return true;
-}
-
-
-unsigned long long nextPrime(unsigned long long value){
-    if (checkPrime(value+1)) return value+1;
-    else nextPrime(value+1);
-}
-
+#include "task2.h"
+#include "task3.h"
 unsigned long long sumPrime(unsigned int hbound){
-
     unsigned long long summ=0;
-
-    int n = 1;
-    while (nextPrime(n)<hbound){
-        n=nextPrime(n);
-        summ+=n;
+    for (unsigned int i=2;i<=hbound;i++)
+    {
+        if(checkPrime(i)) {
+            summ += i;
+        }
     }
-
     return summ;
 }
-
-
