4c42e5476c58abff059d2ea2de52f20894203ae7
milalupehina <70576424+milalupehina@users.noreply.github.com>
2020-10-13 21:42:20 +0300

    t2

---
+++ include/task2.h
@@ -0,0 +1,7 @@
+
+#ifndef LAB1_2_TASK2_H
+#define LAB1_2_TASK2_H
+bool checkPrime(unsigned int value);
+unsigned long long nPrime(unsigned n);
+unsigned long long nextPrime(unsigned long long value);
+#endif //LAB1_2_TASK2_H
