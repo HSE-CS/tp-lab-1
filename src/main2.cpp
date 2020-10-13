919a7721bb80b0d89cbd182a83daf72af63190fd
Stolbov Yaroslav <46395269+CoffeeMan@users.noreply.github.com>
2020-10-13 11:34:19 +0300

    Add files via upload

--- src/main2.cpp
+++ src/main2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << boolalpha << checkPrime(2) << "\n";
-    cout << nPrime(3571) << "\n";
+    cout << nPrime(6) << "\n";
     cout << nextPrime(3);
     return 0;
 }
