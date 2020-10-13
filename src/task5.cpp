c223e5dd4b79ae0c4acbe4249602ea72bd1bd139
Urtyukov Ilya <urtilya@gmai.com>
2020-10-13 13:06:07 +0400

    lab

---
+++ src/task5.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include "task4.h"
+
+using namespace std; 
+void split(char ***result, int *N, char *buf, char ch){
+	
+	
+}
