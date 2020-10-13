0b74a0862f370792a657a7c9ac495d3a9351a1dc
GlebNeshchetkin <glebneshchetkin@gmail.com>
2020-10-13 10:59:44 +0300

    9Test

--- src/task4.cpp
+++ src/task4.cpp
@@ -12,13 +12,7 @@ char* sum(char* x, char* y)
 	int n1 = atoi(x);
 	int n2 = atoi(y);
 	long long int n3 = n1 + n2;
-	//printf("%d", n3);
-	//char* c;
 	char c[10];
-	printf("res=%s\n",_itoa(n3,c,10));
-	//c = (char*)malloc(sizeof(char) * 10);
-	//c = n3 + '/0';
 	char* res = c;
-	printf("%s\n", res);
 	return res;
 }
