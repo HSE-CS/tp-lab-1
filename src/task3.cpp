e3719d17ef2bdbf25da08fe209602f52db2181e0
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-10-13 13:10:06 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -6,7 +6,7 @@
 #include "task2.h"
 
 unsigned long long sumPrime(unsigned int hbound){
-    unsigned long sum = 0;
-    for (unsigned int i = 1; i < hbound; i++) if (checkPrime(i)) sum += i;
+    unsigned long sum = {0};
+    for (unsigned int i = {1}; i < hbound; i++) if (checkPrime(i)) sum += i;
     return sum;
 }
