f5208830affb5f7bb61e717dd57b66e2aea45814
DenisKabanov <dskabanov@edu.hse.ru>
2020-10-13 23:05:33 +0300

    task5

--- src/main5.cpp
+++ src/main5.cpp
@@ -6,30 +6,30 @@ using namespace std;
 
 int main() 
 {
-	int N = 0;
-	char* buf = "111,135,999";
+	int N = 0,k=3;
+	char* buf = "123,456,789";
 	char** result = nullptr;
 	split(&result, &N, buf, ',');
 	cout << result[0] << endl << result[1] << endl << result[2] << endl<<N<< endl;
-	if (strcmp(result[0], "111") == 0) 
+	if (strcmp(result[0], "123") == 0) 
 	{
 		cout << "yes" << endl;
 	}
 	else 
 		cout << "no" << endl;
-	if (strcmp(result[1], "135") == 0)
+	if (strcmp(result[1], "456") == 0)
 	{
 		cout << "yes" << endl;
 	}
 	else
 		cout << "no" << endl;
-	if (strcmp(result[2], "999") == 0)
+	if (strcmp(result[2], "789") == 0)
 	{
 		cout << "yes" << endl;
 	}
 	else
 		cout << "no" << endl;
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < k; i++)
 	{
 		delete[] result[i];
 	}
--- src/task5.cpp
+++ src/task5.cpp
@@ -7,28 +7,28 @@ using namespace std;
 void split(char*** result, int* N, char* buf, char ch) 
 {
 	unsigned int k = 3;
-	char** arr1 = new char* [k];
+	char** arr = new char* [k];
 	for (int i = 0; i < k; i++) 
 	{
-		arr1[i] = new char[k];
+		arr[i] = new char[k];
 	}
-	(*result) = arr1;
+	(*result) = arr;
 	int numoflet = 0;
 	for (int i = 0; i < strlen(buf); i++)
 	{
 		if (buf[i] == ch) 
 		{
-			arr1[(*N)][numoflet] = '\0';
-			(*N)++;
+			arr[(*N)][numoflet] = '\0';
 			numoflet = 0;
+			(*N)++;
 
 			continue;
 		}
 		if (i == strlen(buf) - 1) 
 		{
-			arr1[(*N)][numoflet + 1] = '\0';
+			arr[(*N)][numoflet + 1] = '\0';
 		}
-		arr1[(*N)][numoflet] = buf[i];
+		arr[(*N)][numoflet] = buf[i];
 		numoflet++;
 	}
 	(*N)++;
