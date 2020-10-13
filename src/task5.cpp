1d01dbc77ab2dec75889b978a358aeaaee46fcbd
Eugene <evd.jeka@gmail.com>
2020-10-13 19:06:44 +0300

    fix compilation on older gcc versions

--- src/task5.cpp
+++ src/task5.cpp
@@ -1,9 +1,9 @@
 #include "task5.h"
 #include <stdlib.h>
-#include <iostream>
+#include <stdio.h>
 #include <string.h>
 
-#define CHECK(x) if (!x) {std::cout << "[split] malloc failed" << '\n'; exit(1);}
+#define CHECK(x) if (!x) {fprintf(stderr, "[split] malloc failed"); exit(1);}
 
 void split(char ***result, int *N, const char *buf, char ch){
     char **splits = (char**)malloc(strlen(buf)*sizeof(char*));
@@ -18,7 +18,7 @@ void split(char ***result, int *N, const char *buf, char ch){
             CHECK(new_split);
             current_split[j] = '\0';
             // Thanks, valgrind!
-            current_split = (char*)reallocarray(current_split, j+1, sizeof(char));
+            current_split = (char*)realloc(current_split, (j+1)*sizeof(char));
             CHECK(current_split);
             splits[i] = current_split;
             current_split = new_split;
@@ -30,7 +30,7 @@ void split(char ***result, int *N, const char *buf, char ch){
         }
     } while (*buf++ != '\0');
     free(current_split); // Thanks, valgrind!
-    splits = (char**)reallocarray(splits, i, sizeof(char*));
+    splits = (char**)realloc(splits, i*sizeof(char*));
     CHECK(splits);
     *N = i;
     *result = splits;
