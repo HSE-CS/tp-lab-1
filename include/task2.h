ab16d636b9fda42a7a3ee82a02c2775af80c3000
Alyona Uskova <aluskova01@yandex.ru>
2020-10-13 09:29:52 +0300

    Task2

---
+++ include/task2.h
@@ -0,0 +1,12 @@
+#include <string>
+#ifndef TASK2_H
+#define TASK2_H
+
+using namespace std;
+
+bool checkPrime(unsigned int value);
+unsigned long long nPrime(unsigned n);
+unsigned long long nextPrime(unsigned long long value);
+
+#endif
+
---
+++ src/main2.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+#include "task2.h"
+using namespace std;
+
+int main()
+{
+	unsigned int value = 0;
+	unsigned n = 0;
+	unsigned long long num = 0;
+	printf("Enter value which will be checked\n");
+	cin >> value;
+	printf("Enter n (number of prime num which you would like to see)\n");
+	cin >> n;
+	printf("Enter num (to find the closest prime number)\n");
+	cin >> num;
+	printf("\n%d\n", checkPrime(value));
+	printf("%u\n", nPrime(n));
+	printf("%llu\n", nextPrime(num));
+}
