32287b25a2c844a62e8331e32d78eb5283bbc642
milalupehina <70576424+milalupehina@users.noreply.github.com>
2020-10-13 19:25:50 +0300

    please work 2

---
+++ include/task1.h
@@ -0,0 +1,6 @@
+
+#ifndef LAB1_1_TASK1_H
+#define LAB1_1_TASK1_H
+//unsigned long findValue(unsigned int min, unsigned int max);
+unsigned long findValue(unsigned int min,unsigned max);
+#endif //LAB1_1_TASK1_H
