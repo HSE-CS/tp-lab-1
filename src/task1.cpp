25d77a3b07cea31c40fd69a85cf2c72a23c32a93
DenisKabanov <dskabanov@edu.hse.ru>
2020-10-13 20:10:44 +0300

    task1

---
+++ src/task1.cpp
@@ -0,0 +1,22 @@
+#include "task1.h"
+#include <iostream>
+
+using namespace std;
+
+unsigned long findValue(unsigned int min, unsigned max)
+{
+	int i, ans = 0, result = 1;
+	while (ans==0)
+	{
+		for (i = min; i <= max; i++)
+		{
+			if (result % i != 0)
+				break;
+		}
+		if (i >= max)
+			ans = 1;
+		else
+			result++;
+	}
+	return (result);
+}
