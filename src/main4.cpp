e4caa27fc17669d81a2b6243027584664abe4895
Alyona Uskova <aluskova01@yandex.ru>
2020-10-13 20:05:18 +0300

    Task4

---
+++ src/main4.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+#include "task4.h"
+using namespace std;
+
+int main()
+{
+	//char x[] = "123456789";
+	char x[] = "99999999999999999999";
+	//char y[] = "000000001";
+	char y[] = "1";
+	printf("%s", sum(x, y));
+}
