3137a799c6aee418981b7eb98830f9e344091f16
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-10-13 13:09:08 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -8,7 +8,7 @@ unsigned long findValue(unsigned int min, unsigned max){
     unsigned k = max;
     while(true){
         int check = {0};
-        for (int i = min; i <= max; i++) if ( 0 == k % i) check++;
+        for (int i = min; i <= max; i++) if (0 == k % i) check++;
         if (max - min + 1 == check) return k;
         else k++;
     }
