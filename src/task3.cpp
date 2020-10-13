d7113a89e5316dc577e53f1f25942705f460ab2c
DenisKabanov <dskabanov@edu.hse.ru>
2020-10-13 20:45:48 +0300

    task3

---
+++ src/task3.cpp
@@ -0,0 +1,25 @@
+#include "task3.h"
+#include <iostream>
+
+using namespace std;
+
+bool checkPrime(unsigned int value)
+{
+	for (unsigned int i = 2; i <= sqrt(value); i++)
+		if (value % i == 0)
+			return false;
+	return true;
+}
+
+unsigned long long sumPrime(unsigned int hbound) 
+{
+	long sumprime = 0;
+	for (int i = 2; i < hbound; i++) 
+	{
+		if (checkPrime(i)==true) 
+		{
+			sumprime =sumprime+i;
+		}
+	}
+	return sumprime;
+}
