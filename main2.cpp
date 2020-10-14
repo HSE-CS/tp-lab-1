988acc5bb69fab2d7339e4e51b93fa13be9c6238
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2020-10-14 23:51:30 +0300

    second

---
+++ main2.cpp
@@ -0,0 +1,15 @@
+#include"task2.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << checkPrime(2) << "\n";
+    cout << checkPrime(3) << "\n";
+    cout << checkPrime(12) << "\n";
+    cout << nPrime(6) << "\n";
+    cout << nPrime(500) << "\n";
+    cout << nextPrime(1031) << "\n";
+    cout << nextPrime(3559) << "\n";
+    cout << nextPrime(2) << "\n";
+} 
