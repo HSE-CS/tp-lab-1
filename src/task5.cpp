adb02ffa311b570343569916d1a4f4ac9ee3ff3e
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2020-10-14 22:40:01 +0300

    Update task5.cpp

--- src/task5.cpp
+++ src/task5.cpp
@@ -9,19 +9,50 @@ using namespace std;
 
 void split(char*** result, int* N, char* buf, char ch) {
 	string a = buf;
-	char raz[] = { ch, '\0' };
-	char* s = (char*)calloc(a.length()+1, sizeof(char));
-	s = strtok(buf, raz);
-	*result = (char**)calloc((a.length() + 2), sizeof(char*));
-	int i = 0;
-	while (s) {
-		(*result)[i] = (char*)calloc(strlen(s) + 20, sizeof(char));
-		(*result)[i] = s;
-		//cout << (*result)[i] << endl;
-		s = strtok(NULL, raz);
-		++i;
+	int k = 0;
+	(*result) = (char**)calloc(a.length(), sizeof(char*));
+	int razdel = 0;
+	string buffer;
+	for (int i = 0; i <= a.length(); ++i) {
+		if (a[i] == ch) {
+			for (int j = razdel; j < i; ++j) {
+				buffer.push_back(a[j]);
+			}
+			razdel = i + 1;
+			(*result)[k] = (char*)calloc(buffer.length() + 1, sizeof(char));
+			for (int j = 0; j < buffer.length(); ++j) {
+				(*result)[k][j] = buffer[j];
+			}
+			++k;
+			buffer.clear();
+		}
+		else if (a[i] == '\0') {
+			for (int j = razdel; j < i; ++j) {
+				buffer.push_back(a[j]);
+			}
+			(*result)[k] = (char*)calloc(buffer.length() + 1, sizeof(char));
+			for (int j = 0; j < buffer.length(); ++j) {
+				(*result)[k][j] = buffer[j];
+			}
+			++k;
+			buffer.clear();
+		}
 	}
-	*N = i;
+	*N = k;
+	//string a = buf;
+	//char raz[] = { ch, '\0' };
+	//char* s = (char*)calloc(a.length()+1, sizeof(char));
+	//s = strtok(buf, raz);
+	//*result = (char**)calloc((a.length() + 2), sizeof(char*));
+	//int i = 0;
+	//while (s) {
+	//	(*result)[i] = (char*)calloc(strlen(s) + 20, sizeof(char));
+	//	(*result)[i] = s;
+	//	//cout << (*result)[i] << endl;
+	//	s = strtok(NULL, raz);
+	//	++i;
+	//}
+	//*N = i;
 }
 
 
