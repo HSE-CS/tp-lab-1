6d6c008f38e44a53bc27213144ea3f8390e20f3c
Irina <pasmanikirina29@gmail.com>
2020-10-14 14:45:13 +0300

    tasks try_2

---
+++ src/task2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "task1.h"
+
+unsigned long findValue(unsigned int min, unsigned max) {
+	int flag = 0;
+	int count = 0;
+	int i = min;
+	for (int n = 0; n <= 100000; n++) {
+		i = min;
+		while ((flag == 0) && (i <= max)) {
+			if (n % i == 0) {
+				i++;
+			}
+			else {
+				flag++;
+			}
+		}
+		if (i == max) {
+			return n;
+			break;
+		}
+	}
+}
