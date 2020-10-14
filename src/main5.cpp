469a90984a728e5e64446f1471575185a095eaeb
Shaarlotka <tytylik14@gmail.com>
2020-10-14 13:43:18 +0300

    task5 done

--- src/main5.cpp
+++ src/main5.cpp
@@ -1,1 +1,10 @@
 #include "task5.h"
+
+int main(){
+    char buf[] = "13413,3446,3745735,2455";
+    int N = 0;
+    char **result = nullptr;
+    split(&result, &N, buf, ',');
+    cout << N;
+    return 0;
+}
--- src/task5.cpp
+++ src/task5.cpp
@@ -1,5 +1,24 @@
 #include "task5.h"
 
 void split(char ***result, int *N, char *buf, char ch){
+     for (int i = 0; i < strlen(buf); i++){
+        if (buf[i] == ch){
+            (*N)++;
+        }
+    }
+    (*N)++;
+
+    (*result) = new char*[*N];
+    for (int i = 0; i < (*N); i++){
+        (*result)[i] = new char[strlen(buf)];
+    }
     
+    int count = -1;
+    for (int i = 0; i < (*N); i++){
+        count++;
+        for (int j = 0; buf[count] != ch && buf[count] != '\0'; j++){
+            (*result)[i][j] = buf[count];
+            count++;
+        }
+    }
 }
