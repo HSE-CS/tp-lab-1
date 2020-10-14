936f808b5361e53c777824626d842aac558badbe
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2020-10-15 00:18:26 +0300

    third

---
+++ main3.cpp
@@ -0,0 +1,10 @@
+#include "task3.h"
+#include "task2.h"
+#include <iostream>
+
+// using namespace std;
+
+int main()
+{
+    std::cout << sumPrime(2000000) << "\n";
+} 
---
+++ task3.cpp
@@ -0,0 +1,12 @@
+#include "task3.h"
+#include "task2.h"
+
+unsigned long long sumPrime(unsigned int hbound) {
+	unsigned long long sum = 0;
+	unsigned long long prime = 2;
+	while (prime < hbound) {
+		sum += prime;
+		prime = nextPrime(prime);
+	}
+	return sum;
+} 
