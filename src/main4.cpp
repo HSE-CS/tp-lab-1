3b8f94b4dd618f9f86d5750c93b422d2aff3a92f
__dolt_coder__ <ihackit595@gmail.com>
2020-10-14 17:35:23 +0300

    error from server side

--- src/main4.cpp
+++ src/main4.cpp
@@ -4,8 +4,8 @@ using namespace std;
 #include <cstring>
 
 int main() {
-    char *x="99999999999999999999";
-    char *y="1";
+    // char *x="99999999999999999999";
+    // char *y="1";
     cout<<sum(x, y)<<endl;
     
     return 0;
