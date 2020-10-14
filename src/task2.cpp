01b83ddf8c4f0d07517eb5cc9950107a68f69bb9
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2020-10-14 19:42:38 +0300

    Add files via upload

---
+++ src/task2.cpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+#include "task2.h"
+#include "task3.h"
+
+bool checkPrime(unsigned int value) {
+	if (value == 2)
+		return true;
+	for (unsigned int j = 2; j < value; ++j) {
+		if (value % j == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool checkPrime(unsigned long long value) {
+	if (value == 2)
+		return true;
+	for (int j = 2; j < value; ++j) {
+		if (value % j == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+unsigned long long nPrime(unsigned n) {
+	int i = 0;
+	unsigned long long num = 1;
+	int flag = 1;
+	while (i != n) {
+		++num;
+		for (int j = 2; j < num; ++j) {
+			flag = 1;
+			if (num % j == 0) {
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+			++i;
+	}
+	return num;
+}
+unsigned long long nextPrime(unsigned long long value) {
+	++value;
+	while (!checkPrime(value))
+		++value;
+	return value;
+}
