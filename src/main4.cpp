5b1e26819f3648530f8b187434c52c70d9484671
Nina Titova <nititova@edu.hse.ru>
2020-10-15 01:42:38 +0300

    Titova Task4

---
+++ src/main4.cpp
@@ -0,0 +1,32 @@
+//Реализовать функцию сложения двух сверхдлинных чисел, заданных в виде строк
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "task4.h"
+
+#define SIZE 1000
+
+int main() {
+
+	char n1[SIZE] = "568486";
+	char n2[SIZE] = "9625154";
+	char* ans;
+	long length = 0;
+
+	length = strlen(sum(n1, n2));
+	ans = (char*)malloc(length * sizeof(char));
+	ans = sum(n1, n2);
+
+	for (long i = 0; i < length; i++)
+
+		if ((ans[i] >= '0') && (ans[i] <= '9'))
+
+			printf("%c", ans[i]);
+
+	free(ans);
+
+	return 0;
+}
---
+++ src/task4.cpp
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <malloc.h>
+#include "task4.h"
+using namespace std;
+
+#define SIZE 1000
+
+char* sum(char* x, char* y) {
+
+	int n1[SIZE] = { 0 };
+	int n2[SIZE] = { 0 };
+	char* ans;
+	long len = 0;
+	long j = 0;
+	int k = 0;
+
+	for (long i = strlen(x) - 1; i >= 0; i--) {
+
+		n1[j] = x[i] - '0';
+		j++;
+
+	}
+
+	j = 0;
+
+	for (long i = strlen(y) - 1; i >= 0; i--) {
+
+		n2[j] = y[i] - '0';
+		j++;
+
+	}
+
+	j = 0;
+
+	if (strlen(x) >= strlen(y))
+		len = strlen(x) + 1;
+	else
+		len = strlen(y) + 1;
+
+	for (long i = 0; i < len; i++) {
+
+		n1[i] += n2[i];
+		n1[i + 1] += n1[i] / 10;
+		n1[i] %= 10;
+
+	}
+
+	if (n1[len - 1] == 0)
+		len--;
+
+	for (long i = 0; i < len / 2; i++) {
+
+		k = n1[i];
+		n1[i] = n1[len - 1 - i];
+		n1[len - 1 - i] = k;
+
+	}
+
+	ans = (char*)malloc(len * sizeof(char));
+
+	for (long i = 0; i < len; i++) {
+
+		ans[i] = n1[i] + '0';
+
+	}
+
+	return ans;
+
+	free(n1);
+	free(n2);
+	free(ans);
+
+}
