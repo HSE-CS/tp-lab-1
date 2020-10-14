bb3ed808e5d2d907f2d9413a3818ab7bc8955c88
HabKaffee <neknaum1@gmail.com>
2020-10-14 22:22:17 +0300

    Changed task5

--- src/task5.cpp
+++ src/task5.cpp
@@ -3,25 +3,33 @@
 #include <iostream>
 #include <cstdlib>
 #include <string.h>
-void split(char*** result, int* N, char* buf, char ch) {
-		char* temp = new char[10]();
-		(*result) = new char* [20]();
-		int count = 0;
-		for (int i = 0; i < strlen(buf); i++){
-			if (buf[i] != ch){
-				temp[count] = buf[i];
-				count++;
-			}
-			else {
-				temp[count] = '\0';
-				(*result)[*N] = new char[strlen(buf)]();
-				strcpy((*result)[*N], temp);
-				for (int j = 0; j <= count; j++)
-					temp[j] = 0;
-				count = 0;
-				(*N) += 1;
-			}
+
+void split(char*** result, int* N, char* buf, char ch){
+	unsigned int countDel = 0;
+	*N = 0;
+	for (unsigned int i = 0; i < strlen(buf); i++) {
+		if (buf[i] == ch) {
+			countDel ++;
 		}
-		*N +=1;
-		delete[]temp;
+	}
+	(*result) = new char* [countDel + 2];
+	unsigned int len = 0;
+	for (unsigned int i = 0; i < strlen(buf); i++) {
+		if (buf[i] != ch) {
+			len++;
+		}
+		else {
+			(*result)[*N] = new char[len + 1];
+			strncpy((*result)[*N], buf + i - len, len);
+			(*result)[*N][len] = '\0';
+			len = 0;
+			(*N)++;
+		}
+	}
+	if (len) {
+		(*result)[*N] = new char[len + 1];
+		strncpy((*result)[*N], buf + strlen(buf) - len, len);
+		(*result)[*N][len] = '\0';
+		(*N)++;
+	}
 }
