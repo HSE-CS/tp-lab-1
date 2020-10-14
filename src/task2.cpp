dd59ddc77408864818ce9f7e2334a55b39a44a8b
milalupehina <70576424+milalupehina@users.noreply.github.com>
2020-10-14 18:47:47 +0300

    FIXED

--- src/task2.cpp
+++ src/task2.cpp
@@ -17,26 +17,25 @@ bool checkPrime(unsigned int value)
 }
 unsigned long long nPrime(unsigned n)
 {
-
     int pos = 0;
     for (int i = 1; i < n*n; i++) {
         if(checkPrime(i)) {
-
             pos++;
         }
         if(pos == n+1) return i;
-
     }
-
 }
 
 unsigned long long nextPrime(unsigned long long value)
 {
-    int pos = nPrime(value);
+    for(int i = value+1; i < value*value; i++ )
+    {
+        if(checkPrime(i)) return i;
+    }
+    /*int pos = nPrime(value);
     if(value == 1) return 2;
     if(value == 2) return 3;
     for (int i = pos+1; i < value*value; i++) {
         if(checkPrime(i)) return i;
-    }
-
+    }*/
 }
