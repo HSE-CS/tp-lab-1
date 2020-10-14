dc299ff7e0983d5fce094ae0bca3ffafa9b7c0b9
Anton Klykov <antonklykov01@mail.ru>
2020-10-14 21:32:01 +0300

    New Trying 3

--- src/main4.cpp
+++ src/main4.cpp
@@ -4,8 +4,8 @@ const size_t size_array = 100000;
 
 int main() {
 
-	const char* _number1 = new char[size_array] = { "123456789" };
-	const char* _number2 = new char[size_array] = { "000000001" };
+	const char _number1[size_array] = { "1234" };
+	const char _number2[size_array] = { "0001" };
 	std::cout << sum(_number1, _number2) << std::endl;
 
 	return 0;
