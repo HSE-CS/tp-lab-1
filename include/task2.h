c1c4386c44deff3de8d99f10ffbc9522a73036ea
Irina <pasmanikirina29@gmail.com>
2020-10-14 18:39:02 +0300

    task1,2

---
+++ include/task2.h
@@ -0,0 +1,8 @@
+#ifndef CPLAB1_TASK1_H
+#define CPLAB1_TASK1_H
+
+bool checkPrime(unsigned int value);
+unsigned long long nPrime(unsigned n);
+unsigned long long nextPrime(unsigned long long value);
+
+#endif 
