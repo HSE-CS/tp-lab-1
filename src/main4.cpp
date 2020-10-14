590cd105eb47967adce55a9dd514f676f3df6c9b
Irina <pasmanikirina29@gmail.com>
2020-10-14 19:52:36 +0300

    task1,2,3,4

---
+++ src/main4.cpp
@@ -0,0 +1,11 @@
+#include <cstring>
+#include "task4.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	char y[] = "12345";
+	char x[] = "21";
+	std::cout << sum(x, y);
+	return 0;
+}
---
+++ src/task4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+#include "task4.h"
+
+char* sum(char* x, char* y) {
+	char* res = 0;
+	int len_x = strlen(x);
+	int len_y = strlen(y);
+	char a = ' ', b = ' ';
+	int tmp = 0;
+	int sp = 0;
+
+	if (len_x >= len_y) {
+		char* new_y = (char*)calloc(len_x + 1, sizeof(char));
+		int l = 0; 
+		for (int i = 0; i < len_x; i++) {
+			if (i <= len_y) {
+				new_y[i] = '0';
+			}
+			else {
+				new_y[i] = y[l];
+				l++;
+			}
+		}
+		res = (char*)calloc(len_x + 1, sizeof(char));
+		for (int i = len_x - 1; i > -1; i--) {
+			a = x[i];
+			b = new_y[i];
+			tmp = (a - '0' + b - '0') + sp;
+			res[i] = (tmp % 10) + '0';
+			sp = tmp / 10;
+		}
+		
+		free(new_y);
+	}
+	else {
+		char* new_x = (char*)calloc(len_y + 1, sizeof(char));
+		int l = 0; 
+		for (int i = 0; i < len_y; i++) {
+			if (i <= len_x) {
+				new_x[i] = '0';
+			}
+			else {
+				new_x[i] = x[l];
+				l++;
+			}
+		}
+
+		res = (char*)calloc(len_y + 1, sizeof(char));
+		for (int i = len_y - 1; i > -1; i--) {
+			a = new_x[i];
+			b = y[i];
+			tmp = (a - '0' + b - '0') + sp;
+			res[i] = (tmp % 10) + '0';
+			sp = tmp / 10;
+		}
+		
+		free(new_x);
+	}
+	return res;
+}
