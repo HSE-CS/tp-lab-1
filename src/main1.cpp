6103452b5f76e82a074977b9cf890479ed8d8696
Stifeev Nikita <stifeev.nikita@mail.ru>
2020-10-15 18:14:16 +0500

    lab1

---
+++ src/main1.cpp
@@ -0,0 +1,9 @@
+﻿#include "task1.h"
+
+int main() {
+    unsigned int min, max;
+    min = 1;
+    max = 20;
+    int res = findValue(min, max);
+    cout << res << endl;
+}
---
+++ src/main5.cpp
@@ -0,0 +1,13 @@
+﻿#include "task5.h"
+
+int main()
+{
+    char* buf = "987,654,321";
+    int N = 0;
+    char** result = nullptr;
+    split(&result, &N, buf, ',');
+
+    cout << result[0] << endl;
+    cout << result[1] << endl;
+    cout << result[2] << endl;
+}
