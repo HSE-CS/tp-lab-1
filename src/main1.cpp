6878cd51efe82661713733270032d3019fb4a9a3
MalininDD <54958481+MalininDD@users.noreply.github.com>
2020-10-15 11:07:39 +0300

    Add files via upload

--- src/main1.cpp
+++ src/main1.cpp
@@ -4,12 +4,5 @@
 using namespace std;
 
 int main() {
-  unsigned int min;
-  unsigned int max;
-  cin >> min;
-  cin >> max;
-  unsigned long ans = findValue(min, max);
-  if (ans) {
-    cout << ans << endl;
-  }
+  
 }
