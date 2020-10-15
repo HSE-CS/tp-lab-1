6a29095fa3de48acf5cf81a82acbd9a7f5187ed8
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-10-15 04:51:27 +0300

    fix5

--- src/main5.cpp
+++ src/main5.cpp
@@ -7,13 +7,10 @@ using namespace std;
 
 int main() {
 
-    char buf[] = "111.222.333";
+    char *buf = "123,456,789";
     int N = 0;
     char** result = nullptr;
-    split(&result, &N, buf, '.');
-    for (int i = 0; i < N; i++)
-        cout << result[i]<< endl;
-
+    split(&result, &N, buf, ',');
 
 
     return 0;
