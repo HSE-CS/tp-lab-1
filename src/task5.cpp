3c0f3fdd3bd4e40e139fa519956d54767f24b5f6
Ilya-Tatsenko <66740021+Ilya-Tatsenko@users.noreply.github.com>
2020-10-15 22:53:38 +0300

    Update task5.cpp

--- src/task5.cpp
+++ src/task5.cpp
@@ -1,5 +1,10 @@
 #include"task5.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include<string>
+#include <iostream>
+
+using namespace std;
 
 void split(char*** result, int* N, char* buf, char ch)
 {
