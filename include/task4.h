0b5b2e2201dccf164fc66b46788c1e58908b300e
k-dash6 <71211839+k-dash6@users.noreply.github.com>
2020-10-15 03:42:59 +0300

    emp_1

--- include/task4.h
+++
@@ -1,6 +0,0 @@
-#define TASK4_H
-#ifdef TASK4_H
-
-char* sum(char* x, char* y);
-
-#endif // TASK4_H
--- include/task5.h
+++
@@ -1,8 +0,0 @@
-#define TASK5_H
-#ifdef TASK5_H
-
-using namespace std;
-
-void split(char*** result, int* N, char* buf, char ch);
-
-#endif // TASK5_H
