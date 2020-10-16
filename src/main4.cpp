8c218802e72d1627907f54ce15ed8a6d0c8da85a
Gerasimov Alexey <gerasimov2122@icloud.com>
2020-10-16 19:14:24 +0300

    task4

---
+++ src/main4.cpp
@@ -0,0 +1,7 @@
+#include "task4.h"
+int main(){
+    char *x="99";
+    char *y="1";
+    char *z=sum(x,y);
+    cout<<z<<endl;
+}
---
+++ src/task4.cpp
@@ -0,0 +1,56 @@
+#include <cstring>
+
+char *sum(char *x, char *y) {
+    int maxLength = max(strlen(x), strlen(y));
+    int resultLength;
+    if(x[0]+y[0]-'0'-'0'>9){
+        resultLength = maxLength+1;
+    }else{
+        resultLength = maxLength;
+    }
+    char *resultStr = new char[resultLength];
+    int counterX = strlen(x)-1;
+    int counterY = strlen(y)-1;
+    int counterZ = 0;
+    int overflow = 0;
+    while (counterX >= 0 && counterY >= 0) {
+        int intX = x[counterX] - '0';
+        int intY = y[counterY] - '0';
+        if (intX + intY + overflow > 9) {
+            resultStr[resultLength - counterZ - 1] = ((intX + intY + overflow) % 10) + '0';
+            overflow = 1;
+        } else {
+            resultStr[resultLength - counterZ - 1] = (intX + intY + overflow) + '0';
+            overflow = 0;
+        }
+        counterZ++;
+        counterX--;
+        counterY--;
+    }
+    while (maxLength - counterZ >= 0) {
+        if (counterX >= 0) {
+            if (x[counterX] - '0' + overflow > 9) {
+                resultStr[resultLength - counterZ - 1] = ((x[counterX] - '0' + overflow) % 10) + '0';
+                overflow = 1;
+            } else {
+                resultStr[resultLength - counterZ - 1] = (x[counterX] - '0' + overflow) + '0';
+                overflow = 0;
+            }
+            counterX--;
+        } else if (counterY >= 0) {
+            if (y[counterY] - '0' + overflow > 9) {
+                resultStr[resultLength - counterZ - 1] = ((y[counterY] - '0' + overflow) % 10) + '0';
+                overflow = 1;
+            } else {
+                resultStr[resultLength - counterZ - 1] = (y[counterY] - '0' + overflow) + '0';
+                overflow = 0;
+            }
+            counterY--;
+        } else if (counterX < 0 && counterY < 0) {
+            resultStr[resultLength - counterZ - 1] = overflow + '0';
+        }
+        counterZ++;
+    }
+
+    return resultStr;
+}
