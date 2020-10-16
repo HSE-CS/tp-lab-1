d518bab2b9bd904723e437d430f0c94b7c846e32
Gerasimov Alexey <gerasimov2122@icloud.com>
2020-10-16 19:52:22 +0300

    task5

---
+++ src/task5.cpp
@@ -0,0 +1,29 @@
+#include <cstring>
+
+using namespace std;
+
+void split(char ***result, int *N, char *buf, char ch) {
+    int stringCount = 1;
+    for (int i = 0; i < strlen(buf); ++i) {
+        if (buf[i] == ch) {
+            stringCount++;
+        }
+    }
+
+    char** resultStr = new char* [stringCount];
+    for (int i = 0; i < stringCount; ++i) {
+        resultStr[i] = new char[10];
+    }
+    *result = resultStr;
+    int counter = 0;
+    for (int i = 0; i < strlen(buf); ++i) {
+        if (buf[i] == ch) {
+            (*N)++;
+            counter = 0;
+            continue;
+        }
+        resultStr[*N][counter] = buf[i];
+        counter++;
+    }
+    (*N)++;
+}
