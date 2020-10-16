64ba3ce489e7eeac324241f05808ab152ec892f4
Diana <drsharibzhanova@edu.hse.ru>
2020-10-16 18:02:22 +0300

    Third task

---
+++ include/task3.h
@@ -0,0 +1,5 @@
+#ifndef LAB1_H
+#define LAB1_H
+
+unsigned long long sumPrime(unsigned int hbound);// сумма всех чисел до hbound(не включая его)
+#endif
---
+++ src/main3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "task3.h"
+#include "task2.h"
+
+int main() {
+    int value = { 20 };
+    std::cout << sumPrime(value);
+    return 0;
+}
