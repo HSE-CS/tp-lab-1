33772c6193f7ae5459f1e5ae23f73b16fb9d8117
Karina Mashanova <karmen.01@inbox.ru>
2020-10-17 02:33:07 +0300

    first

--- src/main4.cpp
+++ src/main4.cpp
@@ -1,13 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <algorithm>
 #include "task4.h"
 
 using namespace std;
 
 int main() {
-	cout << sum("999","1") << "\n";
+	cout << sum("990","1") << "\n";
 	return 0;
 }
--- src/task4.cpp
+++ src/task4.cpp
@@ -1,7 +1,4 @@
-#include <malloc.h>
 #include <string.h>
-#include "task4.h"
-
 
 char* sum(char* x, char* y) {
 	unsigned int size_a = strlen(x);
@@ -11,32 +8,28 @@ char* sum(char* x, char* y) {
 		lenght = size_a + 1;
 	}
 	else lenght = size_b + 1;
-	char* res = (char*)malloc(lenght);
+	char* res = new char[lenght];
 	memset(res, ' ', lenght);
 	res[lenght] = '\0';
 	int a, b, z = 0;
-
 	for (int i = 1; i <= lenght; i++) {
-		if (i > size_a) {
-			int a = 0;
-		}
-		else int a = x[size_a - i] - '0';
-		if (i > size_b) {
-			int b = 0;
-		}
-		else int b = y[size_b - i] - '0';
-		int res1 = a + b + z;
-		res[lenght - i] = '0' + res1 % 10;
-		z = res1 / 10;
-		}
+		if (i > size_a)
+			a = 0;
+		else a = x[size_a - i] - '0';
+		if (i > size_b)
+			b = 0;
+		else b = y[size_b - i] - '0';
 
+		int c = a + b + z;
+		res[lenght - i] = '0' + c % 10;
+		z = c / 10;
+	}
 	if (res[0] == '0' && res[1] != '0') {
 		for (int i = 0; i < lenght - 1; i++)
 			res[i] = res[i + 1];
-			res[lenght - 1] = '\0';
-		}
-
-		return res;
+		res[lenght - 1] = '\0';
+	}
+	return res;
 }
 
 
