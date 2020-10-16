fbe58208b7a59afa58f0874dab0b83f281c8f80f
Diana <drsharibzhanova@edu.hse.ru>
2020-10-16 21:55:17 +0300

    Are you kidding?

--- src/main5.cpp
+++ src/main5.cpp
@@ -3,18 +3,20 @@
 
 using namespace std;
 
-int main() {
-    
-	char* buf = strdup("I,4,you!");
-	int N = 0;
-	char** result = nullptr;
-	split(&result, &N, buf, ',');
+int main()
+{
 
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; result[i][j] != '\0'; j++)
-			std::cout << result[i][j];
-		std::cout << endl;
-	}
-	return 0;
+    char* buf = "45,21,34";
+    int N = 0;
+    char** result = nullptr;
+    split(&result, &N, buf, ',');
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; result[i][j] != '\0'; j++)
+            cout << result[i][j];
+        cout << endl;
+    }
+
+
 }
