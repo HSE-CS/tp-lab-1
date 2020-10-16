0e4e78106713df46d43d0d7d3416c9d976578437
Denis Golovanov <dmgolovanov@edu.hse.ru>
2020-10-16 18:12:33 +0300

    Attempt 1

---
+++ src/main1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include "task1.h"
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	return 0;
+}
