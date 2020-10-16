1eb981128b1129e747a408bd42f17b312dba348a
Nina Titova <nititova@edu.hse.ru>
2020-10-16 16:01:10 +0300

    Titova Task5

---
+++ src/task5.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <algorithm>
+//#include <iostream>
+//#include <cstdlib>
+//#include <ctime>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include "task5.h"
+//using namespace std;
+
+void split(char*** result, int* N, char* buf, char ch) {
+
+	int len = strlen(buf);
+	*N = 1;
+	int s = 0;
+	int k = 0;
+	int t = 0;
+	int x = 0;
+
+	for (int i = 0; i < len; i++) {
+
+		if (buf[i] == ch)
+			(*N)++;
+
+	}
+
+	s = *N;
+	int* size = (int*)malloc(s * sizeof(int));
+	(*result) = (char**)malloc(s * sizeof(char*));
+
+	for (int i = 0; i < len; i++) {
+
+		if (buf[i] != ch)
+			k++;
+		else {
+
+			size[t] = k + 1;
+			(*(result))[t] = (char*)malloc(size[t] * sizeof(char));
+			(*(result))[t][size[t] - 1] = '\0';
+			k = 0;
+			t++;
+
+		}
+
+		(*(result))[t] = (char*)malloc((k + 1) * sizeof(char));
+		(*(result))[t][k] = '\0';
+
+	}
+
+	t = 0;
+
+	for (int i = 0; i < len; i++) {
+
+		if (buf[i] != ch)
+			(*result)[t][x++] = buf[i];
+		else {
+
+			x = 0;
+			t++;
+
+		}
+
+	}
+
+}
