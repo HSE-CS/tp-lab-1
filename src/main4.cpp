bfb7b998cfd8465870fca9bb43b8b68d467fe5ca
alex-linyx1 <alextasenko25@gmail.ru>
2020-10-16 23:56:47 +0300

    3

--- src/main4.cpp
+++ src/main4.cpp
@@ -7,7 +7,9 @@ using namespace std;
 int main()
 {
 
+	char s1[] = "99999";
+	char s2[] = "9";
+	cout << sum(s1, s2);
 	return 0;
-
 
 }
--- src/main5.cpp
+++ src/main5.cpp
@@ -6,6 +6,15 @@ using namespace std;
 int main()
 {
 	
+	char buf[] = "11,2342,3343,4434344";
+	int N = 0;
+	char** result = nullptr;
+	split(&result, &N, buf, ' ');
+	cout « N « endl;
+	cout « result[0] « endl;
+	cout « result[1] « endl;
+	cout « result[2] « endl;
+	cout « result[3] « endl;
 	return 0;
 
 }
