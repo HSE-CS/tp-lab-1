60bde7a8ad86ce3bc8d4fb458a9e0990671cee33
EgorBuzanov <egor4ek007@gmail.com>
2020-10-16 22:10:10 +0300

    Egor Buzanov

--- src/task2.cpp
+++ src/task2.cpp
@@ -1,47 +1,12 @@
 #include "task2.h"
 
-long long gcd(long long a, long long b)
-{
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
-}
-
-long long mul(long long a, long long b, long long m)
-{
-    if (b == 1)
-        return a;
-    if (b % 2 == 0)
-    {
-        long long t = mul(a, b / 2, m);
-        return (2 * t) % m;
-    }
-    return (mul(a, b - 1, m) + a) % m;
-}
-
-long long pows(long long a, long long b, long long m)
-{
-    if (b == 0)
-        return 1;
-    if (b % 2 == 0)
-    {
-        long long t = pows(a, b / 2, m);
-        return mul(t, t, m) % m;
-    }
-    return (mul(pows(a, b - 1, m), a, m)) % m;
-}
-
 bool checkPrime(unsigned int value)
 {
     if (value == 2)
         return true;
-    srand(time(NULL));
-    for (int i = 0; i < 100; i++)
+    for (unsigned int i = 2; i < value / 2; i++)
     {
-        long long a = (rand() % (value - 2)) + 2;
-        if (gcd(a, value) != 1)
-            return false;
-        if (pows(a, value - 1, value) != 1)
+        if (value % i)
             return false;
     }
     return true;
--- src/task4.cpp
+++ src/task4.cpp
@@ -14,20 +14,16 @@ char *sum(char *x, char *y)
     char buf = 0;
     for (int i = max_len; i > -1; --i)
     {
+        --x_len;
+        --y_len;
         if (x_len && y_len)
-        {
-            buf = x[x_len - 1] - '0' + y[y_len - 1] - '0';
-            sum[i] = buf % 10 + '0';
-            buf /= 10;
-        }
+            buf = buf + x[x_len] - '0' + y[y_len] - '0';
         else if (x_len)
-        {
-            sum[i] = x[x_len];
-        }
+            buf = buf + x[x_len] - '0';
         else if (y_len)
-        {
-            sum[i] = y[y_len];
-        }
+            buf = buf + y[y_len] - '0';
+        sum[i] = buf % 10 + '0';
+        buf /= 10;
     }
     if (sum[0] == 0)
         return sum + 1;
