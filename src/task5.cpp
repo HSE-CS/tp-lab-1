187543aa9ac9e03d94f8dc8c3a6b54e937f30ebf
Andrew <55204554+Void-fun@users.noreply.github.com>
2020-10-16 19:19:30 +0300

    all should to work

--- src/task5.cpp
+++ src/task5.cpp
@@ -4,34 +4,43 @@
 void split(char*** result, int* N, char* buf, char ch)
 {
 	int length = strlen(buf);
-	*N = countStrings(length);	
 	char** res{ 0 };
-	res = new char* [*N];		
-	//result = (char***)malloc(sizeof(char**));
+	int count_of_razdel{ 0 };
+	for (int i = 0; i < length; i++) {
+		if (buf[i] == ch)
+			count_of_razdel++;
+	}
+	*N = count_of_razdel + 1;
+
+	res = new char* [*N];
 	for (int i = 0; i < *N; i++) {
 		res[i] = 0;
 	}
-	if (res) {
-		//*result = (char**)malloc(*N * sizeof(char*));
-		for (int k = 0; k < *N;) { // sozdaem stroki
-			for (int i = 0; i < length; i++) { // start podstroki
-				for (int j = i; j < length; j++) {  //  end podstroki
-					int len = j - i + 1;  //dlina podstroki
-					char* temp{ 0 };
-					temp = new char[len];
-						for (int l = 0; l < len; l++) {
-								temp[l] = buf[i + l];
-						}
-						res[k] = temp;
-						res[k][len] = 0;
-						k++;
-				}
-			}
+	int last_index{ 0 };
+	int k{ 0 };
+	for (int i = 0; i < length; i++) {
+		if (buf[i] == ch) {
+			int len = i - last_index;
+			res[k] = substing(buf, last_index, len);
+			last_index = i + 1;
+			k++;
 		}
 	}
+	int len = length - last_index;
+	res[k] = substing(buf, last_index, len);
 	(*result) = res;
 }
 
-int countStrings(int length) {
-	return ((1 + length) * length) / 2;
+char* substing(char* buf, int start, int len)
+{
+	char* sub = 0;
+	sub = new char[len+1];
+	for (int i = 0; i < len; i++)
+	{
+		sub[i] = buf[start + i];
+	}
+	sub[len] = '\0';
+	return sub;
 }
+
+
