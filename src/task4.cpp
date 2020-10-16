d3ff9cc44f60063a71ec06039b05d4485a719174
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2020-10-16 19:57:07 +0300

    Update task4.cpp

--- src/task4.cpp
+++ src/task4.cpp
@@ -16,67 +16,57 @@ using namespace std;
 
 char* sum(char* x, char* y) {
 
-	int n1[SIZE] = { 0 };
-	int n2[SIZE] = { 0 };
-	char* ans;
-	long len = 0;
-	long j = 0;
-	int k = 0;
+char* ans;
+long len = 0;
+int num1 = 0;
+int num2 = 0;
+int num3 = 0;
+int b = 0;
+	
+if (strlen(x) >= strlen(y))
+	len = strlen(x);
+else
+	len = strlen(y);
+	
+ans = (char*)malloc((len + 1) * sizeof(char));
+	
+for (int i = 0; i < len + 1; i++) {
+	
+ 	if (strlen(x) <= i)
+      		num1 = 0;
+   	else
+     	 	num1 = x[strlen(x) - i - 1] - '0';
 
-	for (long i = strlen(x) - 1; i >= 0; i--) {
+    	if (strlen(y) <= i)
+      		num2 = 0;
+    	else
+      		num2 = y[strlen(y) - i - 1] - '0';
 
-		n1[j] = x[i] - '0';
-		j++;
+    	num3 = num1 + num2 + b;
 
-	}
+    	b = num3 / 10;
+    	num3 = num3 % 10;
+    	ans[i] = num3 + '0';
 
-	j = 0;
+  }
 
-	for (long i = strlen(y) - 1; i >= 0; i--) {
+if (ans[len] == '0')
+       --len;
 
-		n2[j] = y[i] - '0';
-		j++;
+reverse(ans, ans + len + 1);
 
-	}
+ans[len + 1] = '\0';
+if (len == 0) {
+	   
+   	ans[0] = '0';
+       	ans[1] = '\0';
+	   
+   }
+	
+return ans;
 
-	j = 0;
-
-	if (strlen(x) >= strlen(y))
-		len = strlen(x) + 1;
-	else
-		len = strlen(y) + 1;
-
-	for (long i = 0; i < len; i++) {
-
-		n1[i] += n2[i];
-		n1[i + 1] += n1[i] / 10;
-		n1[i] %= 10;
-
-	}
-
-	if (n1[len - 1] == 0)
-		len--;
-
-	for (long i = 0; i < len / 2; i++) {
-
-		k = n1[i];
-		n1[i] = n1[len - 1 - i];
-		n1[len - 1 - i] = k;
-
-	}
-
-	ans = (char*)malloc(len * sizeof(char));
-
-	for (long i = 0; i < len; i++) {
-
-		ans[i] = n1[i] + '0';
-
-	}
-
-	return ans;
-
-	free(n1);
-	free(n2);
-	free(ans);
+free(n1);
+free(n2);
+free(ans);
 
 }
