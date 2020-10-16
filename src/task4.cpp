4b835d1665bd4d0fa92e427a25975af0e927b9b2
Gerasimov Alexey <gerasimov2122@icloud.com>
2020-10-16 20:56:21 +0300

    another one

--- src/task4.cpp
+++ src/task4.cpp
@@ -1,59 +1,55 @@
 #include <cstring>
 #include <algorithm> 
 
+using namespace std;
 
-char *sum(char *x, char *y) {
-    int maxLength = std::max(strlen(x), strlen(y));
-    int resultLength;
-    if(x[0]+y[0]-'0'-'0'>9){
-        resultLength = maxLength+1;
-    }else{
-        resultLength = maxLength;
-    }
-    char *resultStr = new char[resultLength];
-    int counterX = strlen(x)-1;
-    int counterY = strlen(y)-1;
-    int counterZ = 0;
+char * sum(char *x, char *y){
+    int maxLength = max(strlen(x), strlen(y));
+    int counterX = strlen(x);
+    int counterY = strlen(y);
+    char * buf = new char[maxLength+1];
+    char * t = new char[maxLength];
+
     int overflow = 0;
-    while (counterX >= 0 && counterY >= 0) {
-        int intX = x[counterX] - '0';
-        int intY = y[counterY] - '0';
-        if (intX + intY + overflow > 9) {
-            resultStr[resultLength - counterZ - 1] = ((intX + intY + overflow) % 10) + '0';
-            overflow = 1;
-        } else {
-            resultStr[resultLength - counterZ - 1] = (intX + intY + overflow) + '0';
-            overflow = 0;
+
+    if (counterX > counterY){
+        for (int i = 0; i < counterX - counterY; ++i) {
+            t[i] = '0';
         }
-        counterZ++;
-        counterX--;
-        counterY--;
-    }
-    while (maxLength - counterZ >= 0) {
-        if (counterX >= 0) {
-            if (x[counterX] - '0' + overflow > 9) {
-                resultStr[resultLength - counterZ - 1] = ((x[counterX] - '0' + overflow) % 10) + '0';
-                overflow = 1;
-            } else {
-                resultStr[resultLength - counterZ - 1] = (x[counterX] - '0' + overflow) + '0';
-                overflow = 0;
-            }
-            counterX--;
-        } else if (counterY >= 0) {
-            if (y[counterY] - '0' + overflow > 9) {
-                resultStr[resultLength - counterZ - 1] = ((y[counterY] - '0' + overflow) % 10) + '0';
-                overflow = 1;
-            } else {
-                resultStr[resultLength - counterZ - 1] = (y[counterY] - '0' + overflow) + '0';
-                overflow = 0;
-            }
-            counterY--;
-        } else if (counterX < 0 && counterY < 0) {
-            resultStr[resultLength - counterZ - 1] = overflow + '0';
+        for (int j = 0; j < counterY; ++j) {
+            t[counterX - counterY + j] = y[j];
         }
-        counterZ++;
+        y = t;
     }
-    char* a = "\0";
-    strcat(resultStr, a);
-    return resultStr;
+    if (counterY > counterX){
+        for (int i = 0; i < counterY - counterX; ++i) {
+            t[i] = '0';
+        }
+        for (int j = 0; j < counterX; ++j) {
+            t[counterY - counterX + j] = x[j];
+        }
+        x = t;
+    }
+
+    for (int i = maxLength-1; i > -1; --i) {
+        buf[i] = x[i] + y[i] + overflow - '0';
+        overflow = 0;
+        if (buf[i] > 57){
+            buf[i] = buf[i] - 10;
+            overflow++;
+
+            if (i == 0 && overflow > 0){
+                for (int i = maxLength; i > -1; --i) {
+                    buf[i] = buf[i-1];
+                }
+                buf[0] = '1';
+            }
+        }
+        if (i == 0 && overflow == 0) {
+            buf[maxLength] = '\0';
+        }
+
+    }
+    return buf;
+
 }
