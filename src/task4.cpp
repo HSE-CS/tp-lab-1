4039da648ace489b36653a9feef9e3fd115edc85
Ivanov Daniil <daivanov_6@edu.hse.ru>
2020-10-16 16:36:23 +0300

    lab1 v1.6

--- src/task4.cpp
+++ src/task4.cpp
@@ -6,40 +6,40 @@
 //  Copyright © 2020 Daniil Ivanov. All rights reserved.
 //
 
-#include "task4.h"
-#include <string.h>
-
 char* sum(char* x, char* y)
 {
-    char* resault{ 0 };
+    char* result{ 0 };
     int lenx{ int(strlen(x)) }, leny{ int(strlen(y)) }, remember{ 0 };
     if (lenx >= leny) {
-        char* fake_y = (char*)calloc(lenx+1, sizeof(char));
-        for (int i = 0; i < lenx; i++)
-        {
-            if (i >= leny)
-                fake_y[i] = '0';
-            else
-                fake_y[i] = y[i];
+        char* fake_y = new char[lenx+1]; 
+        if (fake_y) {
+            for (int i = 0; i < lenx + 1; i++)
+            {
+                if (i >= leny)
+                    fake_y[i] = '0';
+                else
+                    fake_y[i] = y[i];
+            }
         }
-        
-        resault = (char*)calloc(lenx+1, sizeof(char)) ;
+
+        result = new char [lenx+1] ;
+        for (int i = 0; i < lenx + 1; i++) {
+            result[i] = 0;
+        }
         for (int i = lenx-1; i > -1 ; i--)
         {
             char a{ x[i] }, b{ fake_y[i] };
             int temp{ (a - '0' + b - '0') + remember };
-            resault[i] = (temp % 10) + '0';
+            result[i] = (temp % 10) + '0';
             remember = temp / 10;
         }
         if (remember == 1)
-            resault[lenx + 1] = 1;
-        else
-            resault++;
+            result[lenx + 1] = 1;
         free(fake_y);
     }
     else {
-        char* fake_x = (char*)calloc(leny+1, sizeof(char)); 
-        for (int i = 0; i < leny; i++)
+        char* fake_x = new char [leny+1];
+        for (int i = 0; i < leny+1; i++)
         {
             if (i >= lenx)
                 fake_x[i] = '0';
@@ -47,19 +47,20 @@ char* sum(char* x, char* y)
                 fake_x[i] = x[i];
         }
 
-        resault = (char*)calloc(leny + 1, sizeof(char));
+        result = new char [leny+1];
+        for (int i = 0; i < leny + 1; i++)
+            result[i] = 0;
+
         for (int i = lenx - 1; i > -1; i--)
         {
             char a{ fake_x[i] }, b{ y[i] };
             int temp{ (a - '0' + b - '0') + remember };
-            resault[i] = (temp % 10) + '0';
+            result[i] = (temp % 10) + '0';
             remember = temp / 10;
         }
         if (remember == 1)
-            resault[lenx + 1] = 1;
-        else
-            resault++;
+            result[lenx + 1] = 1;
         free(fake_x);
     }
-    return resault;
+    return result;
 }
