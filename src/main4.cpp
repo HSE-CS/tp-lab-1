59336f42a06012ac0fbb12d7c2d6fa66eb8690ff
Elizaveta Drobot <lizunchik7@bk.ru>
2020-10-17 01:49:36 +0300

    fix

--- src/main4.cpp
+++ src/main4.cpp
@@ -6,10 +6,7 @@ using namespace std;
 int main() {
     char a []= "123456789";
     char b []= "000000001";
-    int len = strlen(a);
-    cout << len << '\n';
-    cout << a << '\n';
-    cout << b << '\n';
+    
     cout << sum(a, b);
     return 0;
 }
