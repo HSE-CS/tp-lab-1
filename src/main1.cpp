479168f81bbdd7cfc3a977501ea7d299a4b8c7db
Right-bugor <innokentiyyyyy@gmail.com>
2020-10-16 16:44:28 +0300

    try

---
+++ src/main1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include "task1.h"
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	//cout<<findValue(1, 20);
+	return 0;
+}
