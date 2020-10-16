421f8f70b7b2c67fd0387c5c4e86728933f01710
Ivanov Daniil <daivanov_6@edu.hse.ru>
2020-10-16 18:39:48 +0300

    lab1 v1.8

--- src/task4.cpp
+++ src/task4.cpp
@@ -16,29 +16,40 @@ char* sum(char* x, char* y)
     if (lenx >= leny) {
         char* fake_y = new char[lenx+1]; 
         if (fake_y) {
-            for (int i = 0; i < lenx + 1; i++)
+            int zeros = lenx - leny - 1;
+            for (int i = 0; i < lenx+1; i++)
             {
-                if (i >= leny)
-                    fake_y[i] = '0';
+                if (i == lenx)
+                    fake_y[i] = '\0';
+                if (i > zeros)
+                    fake_y[i] = y[i-zeros-1];
                 else
-                    fake_y[i] = y[i];
+                    fake_y[i] = '0';
             }
         }
 
-        result = new char [lenx+1] ;
-        for (int i = 0; i < lenx + 1; i++) {
-            result[i] = 0;
+        result = new char [lenx+2] ;
+        for (int i = 0; i < lenx+2; i++) {
+            if (i == lenx+1)
+                result[i] = '\0';
+            else
+                result[i] = '0';
         }
         for (int i = lenx-1; i > -1 ; i--)
         {
             char a{ x[i] }, b{ fake_y[i] };
             int temp{ (a - '0' + b - '0') + remember };
-            result[i] = (temp % 10) + '0';
+            result[i+1] = (temp % 10) + '0';
             remember = temp / 10;
         }
-        if (remember == 1)
-            result[lenx + 1] = 1;
-        free(fake_y);
+        delete(fake_y);
+        if (remember == 1) {
+            result[0] = '1';
+            return result;
+        }
+        else
+            return result+1;
+        
     }
     else {
         char* fake_x = new char [leny+1];
@@ -63,7 +74,7 @@ char* sum(char* x, char* y)
         }
         if (remember == 1)
             result[lenx + 1] = 1;
-        free(fake_x);
+        delete(fake_x);
     }
     return result;
 }
