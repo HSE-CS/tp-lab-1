4b8024e46167c967367c597660ea2725b9e09102
Мария <71264966+Karcevamd@users.noreply.github.com>
2020-10-16 19:04:15 +0300

    kartsevamd

---
+++ src/main1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include  <vector>
+#include "task1.h"
+
+
+using namespace std;
+int main()
+{
+    int min = 1;
+    int max = 20;
+    int unsigned long c ;
+    c = findValue(min, max);
+    cout << c << endl;
+    return 0;
+}
