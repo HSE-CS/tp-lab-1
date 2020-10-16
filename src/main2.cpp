ca41877251b50cec63612a70f4eb790071f329e6
Gerasimov Alexey <gerasimov2122@icloud.com>
2020-10-16 17:01:37 +0300

    task2

---
+++ src/main2.cpp
@@ -0,0 +1,15 @@
+#include "task2.h"
+
+int main() {
+    cout << checkPrime(1) << endl;
+    cout << checkPrime(2) << endl;
+    cout << checkPrime(357) << endl;
+
+    cout << nPrime(1) << endl;
+    cout << nPrime(2) << endl;
+    cout << nPrime(357) << endl;
+
+    cout << nextPrime(1) << endl;
+    cout << nextPrime(2) << endl;
+    cout << nextPrime(357) << endl;
+}
