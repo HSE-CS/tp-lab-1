cfb9d50828d2ba330c549f70b79edc0fc55b3d77
Мария <71264966+Karcevamd@users.noreply.github.com>
2020-10-16 22:03:51 +0300

    fourth

--- include/task3.h
+++ include/task3.h
@@ -1,5 +1,5 @@
 #ifndef TASK3_H
 #define TASH3_H
-bool checkPrime(unsigned int value);
+//bool checkPrime(unsigned int);
 unsigned long long sumPrime(unsigned int hbound);
 #endif // !TASK3_H
--- src/task3.cpp
+++ src/task3.cpp
@@ -1,11 +1,4 @@
-bool checkPrime(unsigned int value) {
-    for (int j = 2; j * j <= value; j++)
-    {
-        if (value % j == 0)
-            return false;
-    }
-    return true;
-}
+#include "task2.h"
 unsigned long long sumPrime(unsigned int hbound) {
     int long long a = 0;
     for (int i = 0; i< hbound; i++) {
