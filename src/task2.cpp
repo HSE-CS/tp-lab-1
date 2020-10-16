998f9fa3ca4d53858f861f97332a97dc824c890b
EgorBuzanov <egor4ek007@gmail.com>
2020-10-16 21:43:13 +0300

    Egor Buzanov

--- src/task2.cpp
+++ src/task2.cpp
@@ -49,39 +49,14 @@ bool checkPrime(unsigned int value)
 
 unsigned long long nPrime(unsigned n)
 {
-    int size = n;
-    int *primes = new int[n];
-    int *numbers = new int[size];
-    for (int i = 0; i < size; i++)
-        numbers[i] = i;
-    primes[0] = 2;
-    int i = 0;
-    while (i < n)
+    unsigned counter = 0;
+    unsigned int prime_num = 2;
+    while (counter < n)
     {
-        int p = primes[i++];
-        for (int j = p * 2; j < size; j += p)
-            numbers[j] = 0;
-        while (numbers[p + 1] == 0)
-            p++;
-        if (p + 1 >= size)
-        {
-            int *tmp = new int[size * 2];
-
-            for (int k = 0; k < size; k++)
-                tmp[k] = numbers[k];
-            delete[] numbers;
-            size *= 2;
-            numbers = tmp;
-            for (int j = size / 2; j < size; j++)
-                numbers[j] = j;
-            i = 0;
-        }
-        else
-            primes[i] = p + 1;
+        if (checkPrime(prime_num))
+            ++counter;
     }
-    return primes[n - 1];
-    delete[] numbers;
-    delete[] primes;
+    return prime_num;
 };
 
 unsigned long long nextPrime(unsigned long long value)
