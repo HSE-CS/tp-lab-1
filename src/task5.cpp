a775e71f619e85f1b593e56ad9c06602e7c97274
Karina Mashanova <karmen.01@inbox.ru>
2020-10-17 03:08:10 +0300

    first

--- src/task5.cpp
+++ src/task5.cpp
@@ -1,31 +1,27 @@
 #include <cstring>
-
 void split(char*** result, int* N, char* buf, char ch) {
 	int count = 0;
-	for (int i = 0; buf[i] < strlen(buf); i++) {
+	for (int i = 0; buf[i] < strlen(buf); i++)
 		if (buf[i] == ch)
 			count++;
-	}
-
-
 	*result = new char* [count + 1];
 	int last = 0;
 	int i = 0;
 	for (; buf[i] != strlen(buf) - 1; i++) {
 		if (buf[i] == ch) {
 			char* mas = new char[i - last + 1];
-			for (int j = last; j < i; j++) {
+			mas[i - last] = '\0';
+			for (int j = last; j < i; j++)
 				mas[j - last] = buf[j];
-			}
 			(*result)[(*N)] = mas;
 			*N = *N + 1;
 			last = i + 1;
 		}
 	}
 	char* mas = new char[i - last + 1];
-	for (int j = last; j < i; j++) {
+	mas[i - last] = '\0';
+	for (int j = last; j < i; j++)
 		mas[j - last] = buf[j];
-	}
 	(*result)[(*N)] = mas;
 	*N = *N + 1;
 }
