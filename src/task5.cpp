d334198584e0cd5a0b9771e8ca0056c6dfa47031
IrinaZarubina <63425301+IrinaZarubina@users.noreply.github.com>
2020-10-17 17:57:48 +0300

    Update task5.cpp

--- src/task5.cpp
+++ src/task5.cpp
@@ -1,11 +1,9 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
 #include <malloc.h>
-#include <string.h>
+#include <cstring>
 #include "task5.h"
 using namespace std;
 
