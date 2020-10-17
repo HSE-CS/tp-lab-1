e7c728e2fc415bc1a8c9199ac83051b7d71b1fbf
Polina <polpan01@mail.ru>
2020-10-17 17:52:03 +0300

    task1

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,12 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include "task1.h"
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
+#include <math.h>
+#include <string.h>
+#include <malloc.h>
 unsigned long findValue(unsigned int min, unsigned max) {
 	
 	int least = 1;
-	while (true)  {
-		for (int i = max; i <= max; i++) {
+	while (1)  {
+		for (int i = min; i <= max; i++) {
 			if ((least % i == 0) && (i == max))
 				return least;
 			if (least % i != 0)
