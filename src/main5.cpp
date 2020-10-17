ddd3d9a68624dcb523685bd1a1687688877f418b
PolinaZharavina <pdzharavina@edu.hse.ru>
2020-10-17 21:53:35 +0300

    task5

--- src/main5.cpp
+++ src/main5.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
-
+    char *buf="LKJHGFGH,LKJHGFGHJ,LKJHGFDRFGHJK,LKJHGFHJ,JHGFD,OIUYTFD,OIUYTF";
+    int N=0;
+    char **result=nullptr;
+    split(&result, &N, buf, ',');
+    std::cout << result[0]<<"\n"<<result[1]<<"\n"<<result[2]<<"\n"<<N<<"\n";
 	return 0;
 }
