4315a9d13cca1b0819ef56217e25b3a28d060ae8
PolinaZharavina <pdzharavina@edu.hse.ru>
2020-10-18 12:19:11 +0300

    task2 test4_1

--- src/task2.cpp
+++ src/task2.cpp
@@ -11,50 +11,15 @@ bool checkPrime(unsigned int value)
 unsigned long long nPrime(unsigned n)
 {
     
-    int size = n;
-    unsigned long long* primes = (int*)malloc(n * sizeof(int));
-    int* numbers = (int*)malloc(size * sizeof(int));
-   
-
-    for (int i = 0; i < size; i++)
-        numbers[i] = i; 
-
-    primes[0] = 2; 
-    int i = 0; 
-
-    while (i < n) {
-        int p = primes[i++]; 
-
-        for (int j = p * 2; j < size; j += p)
-            numbers[j] = 0; 
-
-        while (numbers[p + 1] == 0)
-            p++; 
-
-        if (p + 1 >= size) { 
-        
-            int* tmp = (int*)malloc(size * 2 * sizeof(int));
-            for (int k = 0; k < size; k++)
-                tmp[k] = numbers[k];
-
-            free(numbers);
-
-            size *= 2;
-            numbers = tmp;
-
-            for (int j = size / 2; j < size; j++)
-                numbers[j] = j; 
-
-            i = 0; 
-        }
-        else
-            primes[i] = p + 1; 
-    }
-    return(primes[n - 1]);
-    
-
-    free(numbers);
-    free(primes);
+     unsigned long long c = 0, k = 0;
+    while (true)
+    {
+        if (c < n)
+        {
+            if (checkPrime(k++))
+            {
+                c++;
+            }
 }
 unsigned long long nextPrime(unsigned long long value)
 {
