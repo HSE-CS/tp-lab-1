385736ffe9b9a55e677e8a8f5a814b0a02a3576f
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-10-18 14:19:51 +0300

    Update task5.cpp

--- src/task5.cpp
+++ src/task5.cpp
@@ -7,12 +7,13 @@
 #include <algorithm>
 using namespace std;
 
-void split(char*** result, int* N, char* buf, char ch) {
+void split(char*** result, int* N, char* buf, char ch){
     unsigned count = {0};
     *N = {0};
-    for (unsigned i = {0}; i < strlen(buf); i++) if (buf[i] == ch) count++;
-    (*result) = new char *[count + 2];
     unsigned len = {0};
+    for (unsigned i = {0}; i < strlen(buf); i++) if (ch == buf[i]) count++;
+    if (0 == count) return;
+    (*result) = new char *[count + 2];
     for (unsigned i = {0}; i < strlen(buf); i++)
         if (ch != buf[i]) len++;
         else {
@@ -20,20 +21,12 @@ void split(char*** result, int* N, char* buf, char ch) {
             strncpy((*result)[*N], buf + i - len, len);
             (*result)[*N][len] = '\0';
             len = {0};
-            if (0 == strcmp((*result)[*N], "") ) {
-                delete[](*result)[*N];
-                (*N)--;
-            }
             (*N)++;
         }
-    if (0 != len) {
-        (*result)[*N] = new char[len+1];
+    if (0 != len){
+        (*result)[*N] = new char[len + 1];
         strncpy((*result)[*N], buf + strlen(buf) - len, len);
         (*result)[*N][len] = '\0';
-        if (0 == strcmp((*result)[*N], "")) {
-            delete[](*result)[*N];
-            (*N)--;
-        }
         (*N)++;
     }
 }
