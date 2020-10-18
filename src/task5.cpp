0db9c0d126cec7aad13d586cf76bdb39b64ffc82
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-10-18 14:55:32 +0300

    Update task5.cpp

--- src/task5.cpp
+++ src/task5.cpp
@@ -4,29 +4,28 @@
 
 #include "task5.h"
 #include <cstring>
-#include <algorithm>
-using namespace std;
+
 
 void split(char*** result, int* N, char* buf, char ch){
-    unsigned count = {0};
-    *N = {0};
-    unsigned len = {0};
-    for (unsigned i = {0}; i < strlen(buf); i++) if (ch == buf[i]) count++;
+    unsigned int count = {0};
+    *N = {0};   // substring
+    unsigned int lenString = {0};
+    for (auto i = 0; i < strlen(buf); i++) if (ch == buf[i]) count++;
     if (0 == count) return;
     (*result) = new char *[count + 2];
-    for (unsigned i = {0}; i < strlen(buf); i++)
-        if (ch != buf[i]) len++;
-        else {
-            (*result)[*N] = new char[len + 1];
-            strncpy((*result)[*N], buf + i - len, len);
-            (*result)[*N][len] = '\0';
-            len = {0};
+    for (auto i = 0; i < strlen(buf); i++)
+        if (ch == buf[i]){
+            (*result)[*N] = new char[lenString + 1];
+            for(unsigned int j = {0}; j < lenString; j++) (*result)[*N][j] = buf[i - lenString + j];
+            (*result)[*N][lenString] = '\0';
+            lenString = {0};
             (*N)++;
         }
-    if (0 != len){
-        (*result)[*N] = new char[len + 1];
-        strncpy((*result)[*N], buf + strlen(buf) - len, len);
-        (*result)[*N][len] = '\0';
+        else lenString++;
+    if (0 != lenString){
+        (*result)[*N] = new char[lenString + 1];
+        for(unsigned int i = {0}; i < lenString; i++) (*result)[*N][i] = buf[strlen(buf) - lenString + i];
+        (*result)[*N][lenString] = '\0';
         (*N)++;
     }
 }
